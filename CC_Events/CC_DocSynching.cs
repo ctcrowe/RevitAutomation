c0a3c5069a8b3baca90bcea75df9c183c557c03c
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-02 10:37:59 -0800

    Update CC_DocSynching.cs

--- CC_Events/CC_DocSynching.cs
+++ CC_Events/CC_DocSynching.cs
@@ -20,11 +20,61 @@ namespace CC_Plugin
         public static void synch(object sender, DocumentSynchronizingWithCentralEventArgs args)
         {
             Document doc = args.Document;
-            using (Transaction t = new Transaction(doc, "Add Families"))
+            using (TranscationGroup tg = new TransactionGroup(doc, "Doc Synching"))
             {
-                t.Start();
-                EmbeddedFamilies.run(doc, "Symbols");
-                t.Commit();
+                tg.Start();
+                using (Transaction t = new Transaction(doc, "Room Data"))
+                {
+                    t.Start();
+                    CollectRoomData(doc);
+                    t.Commit();
+                }
+                using (Transaction t = new Transaction(doc, "Add Families"))
+                {
+                    t.Start();
+                    EmbeddedFamilies.run(doc, "Symbols");
+                    t.Commit();
+                }
+                tg.Commit();
+            }
+        }
+        public static void CollectRoomData(Document doc)
+        {
+            List<Element> InstCollector = new FilteredElementCollector(doc).OfClass(typeof(FamilyInstance)).ToElements().ToList();
+            List<Element> RoomCollector = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms).ToElements().ToList();
+            
+            List<string> Rooms = new List<string>();
+            var Data =  new Dictionary<string, List<string>>();
+            
+            foreach(Element e in RoomCollector)
+            {
+                Room r = e as Room;
+                Rooms.Add(r.Name);
+            }
+            foreach(Element e in InstCollector)
+            {
+                FamilyInstance f = e as FamilyInstance;
+                string id = IDParam.Get(e);
+                if(id != null)
+                {
+                    if(f.Room != null)
+                    {
+                        if(Data.ContainsKey(f.Room.Name))
+                            Data[f.Room.Name].Add(e);
+                        else
+                            Data.Add(f.Room.Name, e);
+                    }
+                    else
+                    {
+                        if(f.FromRoom != null)
+                        {
+                            if(Data.ContainsKey(f.FromRoom.Name))
+                                Data[f.FromRoom.Name].Add(e);
+                            else
+                                Data.Add(f.FromRoom.Name, e);
+                        }
+                    }
+                }
             }
         }
     }
