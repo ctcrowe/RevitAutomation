b8917d5e77a7a13ddfd30b14b31ccc9ab9dc59a5
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-09 09:53:13 -0800

    Update Class1.cs

--- DataAnalysis/Class1.cs
+++ DataAnalysis/Class1.cs
@@ -1,9 +1,3 @@
-﻿/// 1) Array dimensions comparing times of different objects to find related.
-/// 2) Array dimensions reading room names to find relationships (Central File Name, Room Name, Room Number, Phase
-///     Two rooms are only the same if all 3 are the smae
-/// 3) Array dimension reading time in project when placed.
-/// 4) Array dimension reading next object placed and previous object placed (relational objects).
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
