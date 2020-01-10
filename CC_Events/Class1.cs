8aa1f277e796cc710ae4db253af2a83fd6bfc18b
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-10 09:08:57 -0800

    Update Class1.cs

--- CC_Events/Class1.cs
+++ CC_Events/Class1.cs
@@ -1,195 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Xml.Linq;
 using System.IO;
+using System.Reflection;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB.Architecture;
 
-namespace CC_Events
+
+namespace CC_Plugin
 {
-    class Class1
+    internal class MFParam
     {
-        public delegate bool DataQualifier(XDocument xdoc);
-        public static void Count(string folder, string file, int Days, DataQualifier qual)
-        {
-            string[] files = Directory.GetFiles(folder);
-            Dictionary<string, int[]> Data = new Dictionary<string, int[]>();
-            foreach (string f in files)
-            {
-                try
-                {
-                    XDocument doc = XDocument.Load(f);
-                    if (qual(doc))
-                    {
-                        string id = doc.Root.Attribute("EleID").Value;
-                        DateTime date;
-                        DateTime.TryParse(doc.Root.Attribute("PlaceTime").Value, out date);
-                        TimeSpan ts = DateTime.Today - date;
-                        int e = (int)Math.Abs(ts.TotalDays);
-                        if (e < Days)
-                        {
-                            if (Data.ContainsKey(id))
-                            {
-                                Data[id][e]++;
-                            }
-                            else
-                            {
-                                int[] a = new int[Days];
-                                a[e]++;
-                                Data.Add(id, a);
-                            }
-                        }
-                    }
-                }
-                catch { }
-            }
+        private static string Location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private static string SharedParams = Location + "\\CC_SharedParams.txt";
 
-            List<string> lines = new List<string>();
-            foreach (var kvp in Data)
+        private static Param P
+        {
+            get
             {
-                string s = kvp.Key;
-                for (int i = 0; i < kvp.Value.Count(); i++)
-                {
-                    s += "," + kvp.Value[i].ToString();
-                }
-                lines.Add(s);
+                return new Param(
+                    /* Name */              "MasterFormat",
+                    /* Type */              ParamType.int,
+                    /* ID */                new Guid("dc2385d1-4c41-4a81-be07-834d54ed32a6"),
+                    /* Param Group */       "BIMData",
+                    /* Categories */        new BuiltInCategory[2] {
+                                                BuiltInCategory.OST_GenericModels},
+                    /* BuiltinParamGroup*/  BuiltInParameterGroup.PG_DATA,
+                    /* Description */       "A REFERENCE TO THE OBJECTS MASTERFORMAT DIVISION.",
+                    /* Visible */           true,
+                    /* Instance */          false,
+                    /* User Modifiable */   true,
+                    /* Fixed */             true);
             }
-            File.WriteAllLines(file, lines);
         }
-        public static void Average(string folder, string file, string Param, DataQualifier qual)
+        public static void Add(Document doc)
         {
-            string[] files = Directory.GetFiles(folder);
-            Dictionary<string, double[]> Data = new Dictionary<string, double[]>();
-
-            foreach (string f in files)
+            Application app = doc.Application;
+            app.SharedParametersFilename = SharedParams;
+            DefinitionFile DefFile = app.OpenSharedParameterFile();
+            if (doc.IsFamilyDocument)
             {
-                try
-                {
-                    XDocument doc = XDocument.Load(f);
-                    if (qual(doc))
-                    {
-                        string id = doc.Root.Attribute("EleID").Value;
-                        double.TryParse(doc.Root.Attribute(Param).Value, out double x);
-                        if (!double.IsNaN(x))
-                        {
-                            if (Data.ContainsKey(id))
-                            {
-                                Data[id][0] += x;
-                                Data[id][1]++;
-                            }
-                            else
-                            {
-                                Data.Add(id, new double[2] { x, 1 });
-                            }
-                        }
-                    }
-                }
-                catch { }
+                ExternalDefinition def = SetupParam(doc) as ExternalDefinition;
+                if (doc.FamilyManager.get_Parameter(P.ID) == null)
+                    doc.FamilyManager.AddParameter(def, P.BuiltInGroup, false);
             }
-
-            List<string> lines = new List<string>();
-            foreach (var kvp in Data)
-            {
-                double y = kvp.Value[0] / kvp.Value[1];
-                string s = kvp.Key + "," + y.ToString();
-                lines.Add(s);
-            }
-            File.WriteAllLines(file, lines);
         }
-        public static void Compare(string folder, string file, string[] parameters, DataQualifier qual)
+        public static string Get(Document doc)
         {
-            string[] files = Directory.GetFiles(folder);
-            Dictionary<string[], int> Data = new Dictionary<string[], int>();
-
-            foreach (string f in files)
-            {
-                try
-                {
-                    XDocument doc = XDocument.Load(f);
-                    if (qual(doc))
-                    {
-                        bool x = true;
-                        string[] pars = new string[parameters.Count()];
-                        for (int i = 0; i < parameters.Count(); i++)
-                        {
-                            if (doc.Root.Attribute(parameters[i]) != null)
-                            {
-                                pars[i] = parameters[i] + "," + doc.Root.Attribute(parameters[i]).Value;
-                            }
-                            else
-                            {
-                                x = false;
-                                break;
-                            }
-                        }
-                        if (x)
-                        {
-                            if (Data.ContainsKey(pars))
-                                Data[pars]++;
-                            else
-                                Data.Add(pars, 1);
-                        }
-                    }
-                }
-                catch { }
-            }
-
-            List<string> lines = new List<string>();
-            foreach(var kvp in Data)
-            {
-                string[] y = kvp.Key;
-                string s = string.Empty;
-                foreach(string z in y)
-                {
-                    s += z + ",";
-                }
-                s += kvp.Value;
-                lines.Add(s);
-            }
-            File.WriteAllLines(file, lines);
+            return P.Get(doc);
         }
-        public static void Variance(string folder, string file, string param, DataQualifier qual)
+        public static string Get(Family f)
         {
-            string[] files = Directory.GetFiles(folder);
-            Dictionary<string, Dictionary<string, int>> Data = new Dictionary<string, Dictionary<string, int>>();
+            return P.Get(f);
+        }
+        public static string Set(Document doc, int i)
+        {
+            Add(doc);
+            return P.Set(doc, i.ToString());
+        }
+        private static Definition SetupParam(Document doc)
+        {
+            Application app = doc.Application;
+            app.SharedParametersFilename = SharedParams;
+            DefinitionFile df = app.OpenSharedParameterFile();
 
-            foreach (string f in files)
+            if (df.Groups.get_Item(P.ParamGroup) == null)
             {
-                try
-                {
-                    XDocument doc = XDocument.Load(f);
-                    if (qual(doc))
-                    {
-                        string id = doc.Root.Attribute("EleID").Value;
-                        string p = doc.Root.Attribute(param).Value;
-                        if (Data.ContainsKey(id))
-                        {
-                            if (Data[id].ContainsKey(param))
-                                Data[id][param]++;
-                            else
-                                Data[id].Add(param, 1);
-                        }
-                        else
-                        {
-                            Dictionary<string, int> d = new Dictionary<string, int>();
-                            d.Add(param, 1);
-                            Data.Add(id, d);
-                        }
-                    }
-                }
-                catch { }
+                DefinitionGroup group = df.Groups.Create(P.ParamGroup);
+                return group.Definitions.Create(new ExDefOptions(P).opt);
             }
-
-            List<string> lines = new List<string>();
-            foreach (var x in Data)
+            else
             {
-                string s = x.Key;
-                foreach (var y in x.Value)
+                DefinitionGroup group = df.Groups.get_Item(P.ParamGroup);
+                if (df.Groups.get_Item(P.ParamGroup).Definitions.get_Item(P.name) == null)
                 {
-                    s += "," + y.Key + " : " + y.Value.ToString();
+                    return group.Definitions.Create(new ExDefOptions(P).opt);
                 }
-                lines.Add(s);
+                else
+                {
+                    return group.Definitions.get_Item(P.name);
+                }
             }
-            File.WriteAllLines(file, lines);
         }
     }
 }
