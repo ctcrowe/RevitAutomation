1cce7bc4470aa59fb1b5fec076a10bfbb94cc6ba
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-13 09:38:02 -0800

    Update Class1.cs

--- DataAnalysis/Class1.cs
+++ DataAnalysis/Class1.cs
@@ -108,15 +108,74 @@ namespace DataAnalysis
                     string ele = doc.Root.Attribute("Name").Value;
                     if(!string.IsNullOrEmpty(ele))
                     {
-                        if(doc.Root.Attribute("Category") != null)
-                        {
-                            string cat = doc.Root.Attribute("Category").Value;
-                            data.Add(ele + '\t' + cat);
-                        }
+                        List<string> title = SplitTitle(ele);
+                        foreach(string s in title)
+                            data.Add(s);
                     }
                 }
             }
             return data;
+        }
+        public static List<string> SplitTitle(string s)
+        {
+            List<string> data = new List<string>();
+            int b = 0;
+            char[] cs = s.ToCharArray();
+            for(int i = 0; i < cs.Count(); i++)
+            {
+                if(!char.IsLetter(cs[i]) || char.IsUpper(cs[i]))
+                {
+                    if(i > b)
+                    {
+                        string z = string.Empty;
+                        for(int j = b; j < i; j++)
+                        {
+                            z += cs[j];
+                        }
+                        data.Add(z);
+                    }
+                    if(!char.IsLetter(cs[i]))
+                        b = i + 1;
+                    else
+                        b = i;
+                }
+            }
+            return data;
+        }
+        public static void run()
+        {
+            string filedir = string.Empty;
+            string filename = string.Empty;
+
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.InitialDirectory = "c:\\";
+                ofd.Filter = "All files (*.*)|*.*";
+                ofd.RestoreDirectory = true;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    filedir = ofd.FileName.TrimEnd(ofd.FileName.Split('\\').Last().ToCharArray());
+                }
+                Console.WriteLine(filedir);
+            }
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.InitialDirectory = "c:\\";
+                sfd.Filter = "All files (*.*)|*.*";
+                sfd.RestoreDirectory = true;
+
+                if(sfd.ShowDialog() == DialogResult.OK)
+                {
+                    if (sfd.FileName.EndsWith(".txt"))
+                        filename = sfd.FileName;
+                    else
+                        filename = sfd.FileName.Split('.').FirstOrDefault() + ".txt";
+                }
+                Console.WriteLine(filename);
+            }
+            var data = GetData(filedir);
+            File.WriteAllLines(filename, data);
         }
     }
 }
