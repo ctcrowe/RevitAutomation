545b021cf27ec8ff5868fca5b5e363c8225bec63
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-16 10:31:21 -0800

    Update Class1.cs

--- CC_Library/Class1.cs
+++ CC_Library/Class1.cs
@@ -6,19 +6,6 @@ using System.Threading.Tasks;
 
 namespace CC_Library
 {
-    public class Prediction
-    {
-        public string Word { get; }
-        public int[] Predictions { get; set; }
-        public int[] Previous { get; set; }
-
-        public Prediction(string s)
-        {
-            this.Word = s;
-            this.Predictions = new int[26];
-            this.Previous = new int[26];
-        }
-    }
     public class WordSections
     {
         private static readonly string directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
