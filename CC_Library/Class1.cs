ff13fd2b9e67198ec44455f6aebc6dfeb1e1f19b
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-17 08:59:04 -0800

    Update Class1.cs

--- CC_Library/Class1.cs
+++ CC_Library/Class1.cs
@@ -78,45 +78,6 @@ namespace CC_Library
                 }
             }
         }
-        public static List<string> SplitTitle(string s)
-        {
-            List<string> data = new List<string>();
-            int b = 0;
-            char[] cs = s.ToCharArray();
-            for (int i = 1; i < cs.Count(); i++)
-            {
-                if (!char.IsLetter(cs[i]))
-                {
-                    if (i > b && b < cs.Count())
-                    {
-                        string z = string.Empty;
-                        for (int j = b; j < i; j++)
-                        {
-                            z += cs[j];
-                        }
-                        data.Add(z);
-                    }
-                    b = i + 1;
-                }
-                else
-                {
-                    if (char.IsUpper(cs[i]) && !char.IsUpper(cs[i - 1]))
-                    {
-                        if (i > b && b < cs.Count())
-                        {
-                            string z = string.Empty;
-                            for (int j = b; j < i; j++)
-                            {
-                                z += cs[j];
-                            }
-                            data.Add(z);
-                        }
-                        b = i;
-                    }
-                }
-            }
-            return data;
-        }
         public static void run()
         {
             string filedir = string.Empty;
