18946b922d1e4db34c8b03057cc472aee8595a79
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-20 15:21:18 -0800

    Update CC_PredictionElement.cs

--- CC_Library/CC_PredictionElement.cs
+++ CC_Library/CC_PredictionElement.cs
@@ -10,5 +10,115 @@
             this.Word = s;
             this.Predictions = new int[26];
         }
+        
+        public static List<PredictionElement> GetData(string folder)
+        {
+            string[] Files = Directory.GetFiles(folder);
+            var data = new List<string>();
+
+            foreach (string f in Files)
+            {
+                XDocument doc = XDocument.Load(f);
+                if (doc.Root.Attribute("Name") != null)
+                {
+                    string ele = doc.Root.Attribute("Name").Value;
+                    if (!string.IsNullOrEmpty(ele))
+                    {
+                        List<string> title = TitleAnalysis.SplitTitle(ele);
+                        foreach (string s in title)
+                            if (!data.Contains(s))
+                                data.Add(s);
+                    }
+                }
+            }
+            if (File.Exists(xfile))
+            {
+                XDocument doc = XDocument.Load(xfile);
+                foreach (XElement ele in doc.Root.Elements())
+                {
+                    data.Add(ele.Attribute("Value").Value);
+                }
+            }
+            return data;
+        }
+        
+        public List<PredictionElement> SplitTitle()
+        {
+            var data = new List<PredictionElement>();
+            int b = 0;
+            char[] cs = this.Title.ToCharArray();
+            for (int i = 1; i < cs.Count(); i++)
+            {
+                if (!char.IsLetter(cs[i]))
+                {
+                    if (i > b && b < cs.Count())
+                    {
+                        string z = string.Empty;
+                        for (int j = b; j < i; j++)
+                        {
+                            z += cs[j];
+                        }
+                        data.Add(new PredictionElement(z));
+                    }
+                    b = i + 1;
+                }
+                else
+                {
+                    if (char.IsUpper(cs[i]) && !char.IsUpper(cs[i - 1]))
+                    {
+                        if (i > b && b < cs.Count())
+                        {
+                            string z = string.Empty;
+                            for (int j = b; j < i; j++)
+                            {
+                                z += cs[j];
+                            }
+                            data.Add(new PredictionElement(z));
+                        }
+                        b = i;
+                    }
+                }
+            }
+            return data;
+        }
+        public static List<PredictionElement> SplitTitle(string s)
+        {
+            var data = new List<PredictionElement>();
+            int b = 0;
+            char[] cs = s.ToCharArray();
+            for (int i = 1; i < cs.Count(); i++)
+            {
+                if (!char.IsLetter(cs[i]))
+                {
+                    if (i > b && b < cs.Count())
+                    {
+                        string z = string.Empty;
+                        for (int j = b; j < i; j++)
+                        {
+                            z += cs[j];
+                        }
+                        data.Add(new PredictionElement(z));
+                    }
+                    b = i + 1;
+                }
+                else
+                {
+                    if (char.IsUpper(cs[i]) && !char.IsUpper(cs[i - 1]))
+                    {
+                        if (i > b && b < cs.Count())
+                        {
+                            string z = string.Empty;
+                            for (int j = b; j < i; j++)
+                            {
+                                z += cs[j];
+                            }
+                            data.Add(new PredictionElement(z));
+                        }
+                        b = i;
+                    }
+                }
+            }
+            return data;
+        }
     }
 }
