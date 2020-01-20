ab682cd972327e1742460274753fb70c07d60954
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-20 10:34:14 -0800

    Update CC_PredictionElement.cs

--- CC_Library/CC_PredictionElement.cs
+++ CC_Library/CC_PredictionElement.cs
@@ -4,13 +4,11 @@
     {
         public string Word { get; }
         public int[] Predictions { get; set; }
-        public int[] Previous { get; set; }
 
         public PredictionElement(string s)
         {
             this.Word = s;
             this.Predictions = new int[26];
-            this.Previous = new int[26];
         }
     }
 }
