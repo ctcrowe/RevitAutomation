173789254b6da5a93dfa268d3c4fcd2df339aa64
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-20 16:10:11 -0800

    Update TitleAnalysis.cs

--- CC_Library/TitleAnalysis.cs
+++ CC_Library/TitleAnalysis.cs
@@ -10,5 +10,35 @@
             this.Title = s;
             this.Section = i;
         }
+        
+        public static List<TitleAnalysis> GetData(string folder)
+        {
+            string[] Files = Directory.GetFiles(folder);
+            var data = new List<TitleAnalysis>();
+
+            foreach (string f in Files)
+            {
+                XDocument doc = XDocument.Load(f);
+                if (doc.Root.Attribute("Name") != null)
+                {
+                    string ele = doc.Root.Attribute("Name").Value;
+                    if (!string.IsNullOrEmpty(ele))
+                    {
+                        if (!data.Any(x => x.Title == ele))
+                        {
+                            if(doc.Root.Attribute("Section") != null)
+                            {
+                                data.Add(new TitleAnalysis(ele, doc.Root.Attribute("Section").Value));
+                            }
+                            else
+                            {
+                                data.Add(new TitleAnalysis(ele, 0));
+                            }
+                        }
+                    }
+                }
+            }
+            return data;
+        }
     }
 }
