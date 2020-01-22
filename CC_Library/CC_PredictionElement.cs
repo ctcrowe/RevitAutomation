1c7d2a370e3aa7a40ef65facbd8ed6ff897dbf2a
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-22 15:04:41 -0800

    Update CC_PredictionElement.cs

--- CC_Library/CC_PredictionElement.cs
+++ CC_Library/CC_PredictionElement.cs
@@ -14,34 +14,32 @@ namespace CC_Library
         public const int PredictionCount = 48;
         public string Word { get; }
         public double[] Predictions { get; set; }
+        private int PredictionNumber { get; set; }
 
         public PredictionElement(string s)
         {
             this.Word = s;
             this.Predictions = new double[PredictionCount];
+            PredictionNumber = 1;
         }
         
-        public void AdjustPredictions(double Max, int Guess, int Correct)
+        public void AdjustPredictions(double Certainty, int Guess, int Correct)
         {
-            double estimate = Math.Abs(Max - 50);
-            if(Guess == Correct)
+            double MaxChange = 1 / PredictionNumber + 1;
+            if(Guess == Correct && Certainty > 0.75)
             {
-                Predictions[Correct] += estimate;
-                for(int i = 0; i < Predictions.Count(); i++)
-                {
-                    if(i != Correct)
-                        Predictions[i] -= (estimate / PredictionCount);
-                }
+                Predictions[Correct] += MaxChange / 2;
             }
             else
             {
-                Predictions[Correct] += estimate;
+                Predictions[Correct] += MaxChange;
                 for(int i = 0; i < Predictions.Count(); i++)
                 {
                     if(i != Correct)
-                       Predictions[i] -= (estimate / PredictionCount);
+                       Predictions[i] -= (MaxChange / PredictionCount);
                 }
             }
+            PredictionNumber += 1;
         }
         
         public static List<PredictionElement> GetData(string folder)
