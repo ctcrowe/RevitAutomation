5b0662d56e39e4f2a5daf7d87e2acd4b19b5ab85
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-28 13:49:26 -0800

    Update Class1.cs

--- CC_Events/Class1.cs
+++ CC_Events/Class1.cs
@@ -75,4 +75,70 @@ namespace CC_Plugin
             }
         }
     }
+        internal class MFConfirmParam
+    {
+        private static string Location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private static string SharedParams = Location + "\\CC_SharedParams.txt";
+
+        private static Param P
+        {
+            get
+            {
+                return new Param(
+                    /* Name */              "MasterFormat",
+                    /* Type */              ParamType.Bool,
+                    /* ID */                new Guid("98d1341d-cf2a-4701-8f3b-772496234477"),
+                    /* Param Group */       "BIMData",
+                    /* Categories */        new BuiltInCategory[1] {
+                                                BuiltInCategory.OST_GenericModel},
+                    /* BuiltinParamGroup*/  BuiltInParameterGroup.PG_DATA,
+                    /* Description */       "CONFIRMS IF THE MASTERFORMAT DIVISION WAS SET AUTOMATICALLY OR THROUGH THE API.",
+                    /* Visible */           true,
+                    /* Instance */          false,
+                    /* User Modifiable */   false,
+                    /* Fixed */             true);
+            }
+        }
+        public static void Add(Document doc)
+        {
+            RevitParamEdits.Add_FamilyParam(doc, P);
+        }
+        public static string Get(Document doc)
+        {
+            return P.Get(doc);
+        }
+        public static string Get(Family f)
+        {
+            return P.Get(f);
+        }
+        public static string Set(Document doc, int i)
+        {
+            Add(doc);
+            return P.Set(doc, i.ToString());
+        }
+        private static Definition SetupParam(Document doc)
+        {
+            Application app = doc.Application;
+            app.SharedParametersFilename = SharedParams;
+            DefinitionFile df = app.OpenSharedParameterFile();
+
+            if (df.Groups.get_Item(P.ParamGroup) == null)
+            {
+                DefinitionGroup group = df.Groups.Create(P.ParamGroup);
+                return group.Definitions.Create(new ExDefOptions(P).opt);
+            }
+            else
+            {
+                DefinitionGroup group = df.Groups.get_Item(P.ParamGroup);
+                if (df.Groups.get_Item(P.ParamGroup).Definitions.get_Item(P.name) == null)
+                {
+                    return group.Definitions.Create(new ExDefOptions(P).opt);
+                }
+                else
+                {
+                    return group.Definitions.get_Item(P.name);
+                }
+            }
+        }
+    }
 }
