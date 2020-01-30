39457ab6c7ccaeebab3dbc925afd24fa16a37f8a
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-30 09:06:21 -0800

    Update CC_MFConfirmParam.cs

--- CC_Events/CC_MFConfirmParam.cs
+++ CC_Events/CC_MFConfirmParam.cs
@@ -37,29 +37,119 @@ namespace CC_Plugin
             Add(doc);
             return P.Set(doc, i.ToString());
         }
-        private static Definition SetupParam(Document doc)
-        {
-            Application app = doc.Application;
-            app.SharedParametersFilename = SharedParams;
-            DefinitionFile df = app.OpenSharedParameterFile();
+    }
+    internal class WidthParam
+    {
+        private static string Location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private static string SharedParams = Location + "\\CC_SharedParams.txt";
 
-            if (df.Groups.get_Item(P.ParamGroup) == null)
+        private static Param P
+        {
+            get
             {
-                DefinitionGroup group = df.Groups.Create(P.ParamGroup);
-                return group.Definitions.Create(new ExDefOptions(P).opt);
+                return new Param(
+                    /* Name */              "oW",
+                    /* Type */              ParamType.Length,
+                    /* ID */                new Guid("7c53c6ed-278f-4036-b5c7-eac8437ab28a"),
+                    /* Param Group */       "Dimensions",
+                    /* Categories */        new BuiltInCategory[8] {
+                                                BuiltInCategory.OST_GenericModel,
+                                                BuiltInCategory.OST_SpecialityEquipment,
+                                                BuiltInCategory.OST_Furniture,
+                                                BuiltInCategory.OST_FurnitureSystems,
+                                                BuiltInCategory.OST_Walls,
+                                                BuiltInCategory.OST_Windows,
+                                                BuiltInCategory.OST_Doors,
+                                                BuiltInCategory.OST_Casework },
+                    /* BuiltinParamGroup*/  BuiltInParameterGroup.PG_CONSTRUCTION,
+                    /* Description */       "USED TO DEFINE THE OVERALL OUTSIDE MAXIMUM DIMENSIONS OF AN OBJECT, REFERENCING LEFT AND RIGHT EXTREMES",
+                    /* Visible */           true,
+                    /* Instance */          false,
+                    /* User Modifiable */   true,
+                    /* Fixed */             true);
             }
-            else
+        }
+        public static void Add(Document doc) { RevitParamEdits.Add_FamilyParam(doc, P); }
+        public static string Set(Document doc, int i)
+        {
+            Add(doc);
+            return P.Set(doc, i.ToString());
+        }
+    }
+    internal class DepthParam
+    {
+        private static string Location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private static string SharedParams = Location + "\\CC_SharedParams.txt";
+
+        private static Param P
+        {
+            get
             {
-                DefinitionGroup group = df.Groups.get_Item(P.ParamGroup);
-                if (df.Groups.get_Item(P.ParamGroup).Definitions.get_Item(P.name) == null)
-                {
-                    return group.Definitions.Create(new ExDefOptions(P).opt);
-                }
-                else
-                {
-                    return group.Definitions.get_Item(P.name);
-                }
+                return new Param(
+                    /* Name */              "oD",
+                    /* Type */              ParamType.Length,
+                    /* ID */                new Guid("93acc448-a229-4d27-956f-c39ffd40c3c3"),
+                    /* Param Group */       "Dimensions",
+                    /* Categories */        new BuiltInCategory[8] {
+                                                BuiltInCategory.OST_GenericModel,
+                                                BuiltInCategory.OST_SpecialityEquipment,
+                                                BuiltInCategory.OST_Furniture,
+                                                BuiltInCategory.OST_FurnitureSystems,
+                                                BuiltInCategory.OST_Walls,
+                                                BuiltInCategory.OST_Windows,
+                                                BuiltInCategory.OST_Doors,
+                                                BuiltInCategory.OST_Casework },
+                    /* BuiltinParamGroup*/  BuiltInParameterGroup.PG_CONSTRUCTION,
+                    /* Description */       "USED TO DEFINE THE OVERALL OUTSIDE MAXIMUM DIMENSIONS OF AN OBJECT, REFERENCING FRONT AND BACK EXTREMES",
+                    /* Visible */           true,
+                    /* Instance */          false,
+                    /* User Modifiable */   true,
+                    /* Fixed */             true);
             }
+        }
+        public static void Add(Document doc) { RevitParamEdits.Add_FamilyParam(doc, P); }
+        public static string Set(Document doc, int i)
+        {
+            Add(doc);
+            return P.Set(doc, i.ToString());
+        }
+    }
+    internal class HeightParam
+    {
+        private static string Location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private static string SharedParams = Location + "\\CC_SharedParams.txt";
+
+        private static Param P
+        {
+            get
+            {
+                return new Param(
+                    /* Name */              "oH",
+                    /* Type */              ParamType.Length,
+                    /* ID */                new Guid("f306ae6d-f153-47e1-b7e1-ac451eada6f2"),
+                    /* Param Group */       "Dimensions",
+                    /* Categories */        new BuiltInCategory[8] {
+                                                BuiltInCategory.OST_GenericModel,
+                                                BuiltInCategory.OST_SpecialityEquipment,
+                                                BuiltInCategory.OST_Furniture,
+                                                BuiltInCategory.OST_FurnitureSystems,
+                                                BuiltInCategory.OST_Walls,
+                                                BuiltInCategory.OST_Windows,
+                                                BuiltInCategory.OST_Doors,
+                                                BuiltInCategory.OST_Casework },
+                    /* BuiltinParamGroup*/  BuiltInParameterGroup.PG_CONSTRUCTION,
+                    /* Description */       "USED TO DEFINE THE OVERALL OUTSIDE MAXIMUM DIMENSIONS OF AN OBJECT, REFERENCING TOP AND BOTTOM EXTREMES",
+                    /* Visible */           true,
+                    /* Instance */          false,
+                    /* User Modifiable */   true,
+                    /* Fixed */             true);
+            }
+        }
+        public static void Add(Document doc) { RevitParamEdits.Add_FamilyParam(doc, P); }
+        public static string Set(Document doc, int i)
+        {
+            Add(doc);
+            return P.Set(doc, i.ToString());
         }
     }
 }
