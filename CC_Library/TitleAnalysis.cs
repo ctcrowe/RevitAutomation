676e782432a6fa6a9ffa8713c95a38f36a1e1636
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-01-31 08:35:04 -0800

    Update TitleAnalysis.cs

--- CC_Library/TitleAnalysis.cs
+++ CC_Library/TitleAnalysis.cs
@@ -7,55 +7,6 @@ using System.Reflection;
 
 namespace CC_Library
 {
-    public class TitleAnalysisPrediction
-    {
-        public delegate void TEST(string s);
-        private static XDocument XDoc
-        {
-            get
-            {
-                Assembly a = typeof(TitleAnalysisPrediction).Assembly;
-
-                string name = a.GetManifestResourceNames().Where(x => x.EndsWith("1.xml")).First();
-                Stream s = a.GetManifestResourceStream(name);
-                XDocument doc = XDocument.Load(s);
-                //s.Close();
-                return doc;
-            }
-        }
-        private static List<PredictionElement> Predictions(List<string> Words)
-        {
-            List<PredictionElement> pes = new List<PredictionElement>();
-            foreach (string s in Words)
-            {
-                XDocument doc = XDoc;
-                if (doc.Root.Elements().Any(x => x.Attribute("Word").Value == s))
-                {
-                    XElement ele = doc.Root.Elements().Where(x => x.Attribute("Word").Value == s).First();
-                    double[] values = new double[ele.Elements().Count()];
-                    for (int i = 0; i < values.Count(); i++)
-                    {
-                        values[i] = double.Parse(ele.Elements().Where(x => x.Attribute("Number").Value == i.ToString()).First().Attribute("Value").Value);
-                    }
-                    PredictionElement pe = new PredictionElement(s, values);
-                    pes.Add(pe);
-                }
-                else
-                {
-                    pes.Add(new PredictionElement(s));
-                }
-            }
-            return pes;
-        }
-        public static int GenPrediction(string Title, TEST t)
-        {
-            List<string> words = TitleAnalysis.SplitTitleWords(Title);
-            List<PredictionElement> preds = Predictions(words);
-            double[] vals = TitleAnalysis.GetPrediction(preds);
-            int p = Array.IndexOf(vals, vals.Max());
-            return p;
-        }
-    }
     public class TitleAnalysis
     {
         public string Title { get; }
