284a541c5a8a84d1d5fbb2f2ba8bda2921ba7550
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-02-04 14:31:54 -0800

    Update CC_FamParam.cs

--- CC_Events/CC_FamParam.cs
+++ CC_Events/CC_FamParam.cs
@@ -33,20 +33,11 @@ namespace CC_Plugin
         public static void Add(object sender, ViewActivatedEventArgs args)
         {
             Document doc = args.Document;
-            
-            if(doc.IsFamilyDocument)
+            using(Transaction t = new Transaction(doc, "Add Family"))
             {
-                using(Transaction t = new Transaction(doc, "Add Family"))
-                {
-                    t.Start();
-                    Application app = doc.Application;
-                    app.SharedParametersFilename = SharedParams;
-                    DefinitionFile DefFile = app.OpenSharedParameterFile();
-                    ExternalDefinition def = SetupParam(doc) as ExternalDefinition;
-                    if (doc.FamilyManager.get_Parameter(P.ID) == null)
-                        doc.FamilyManager.AddParameter(def, P.BuiltInGroup, false);
-                    t.Commit();
-                }
+                t.Start();
+                RevitParamEdits.Add_FamilyParam(doc, P);
+                t.Commit();
             }
         }
         public static string Get(Document doc)
@@ -69,30 +60,6 @@ namespace CC_Plugin
         {
             Add(doc);
             return P.Set(doc, s);
-        }
-        private static Definition SetupParam(Document doc)
-        {
-            Application app = doc.Application;
-            app.SharedParametersFilename = SharedParams;
-            DefinitionFile df = app.OpenSharedParameterFile();
-
-            if (df.Groups.get_Item(P.ParamGroup) == null)
-            {
-                DefinitionGroup group = df.Groups.Create(P.ParamGroup);
-                return group.Definitions.Create(new ExDefOptions(P).opt);
-            }
-            else
-            {
-                DefinitionGroup group = df.Groups.get_Item(P.ParamGroup);
-                if (df.Groups.get_Item(P.ParamGroup).Definitions.get_Item(P.name) == null)
-                {
-                    return group.Definitions.Create(new ExDefOptions(P).opt);
-                }
-                else
-                {
-                    return group.Definitions.get_Item(P.name);
-                }
-            }
         }
     }
 }
