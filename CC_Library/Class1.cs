fda8a289e2de62d48b6870a68c403d6141951792
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-02-05 09:19:50 -0800

    Update Class1.cs

--- CC_Library/Class1.cs
+++ CC_Library/Class1.cs
@@ -9,25 +9,40 @@ using System.Reflection;
 namespace CC_Library
 {
     internal class PredictionPhrase
-        {
-            public string Phrase { get; }
-            public List<PredictionElement> Elements { get; set; }
-            public int Prediction { get; set; }
+    {
+        public string Phrase { get; }
+        public List<PredictionElement> Elements { get; set; }
+        public int Prediction { get; set; }
                 
-            public PredictionPhrase(string p)
+        public PredictionPhrase(string p)
+        {
+            this.Phrase = p;
+            this.Elements = GetElements();
+            this.Prediction = 0;
+        }
+        private List<PredictionElement> GetElements()
+        {
+            var Elements = new List<PredictionElement>();
+            int b = 0;
+            char[] cs = Phrase.ToCharArray();
+            for (int i = 1; i < cs.Count(); i++)
             {
-                this.Phrase = p;
-                this.Elements = GetElements();
-                this.Prediction = 0;
-            }
-            private List<PredictionElement> GetElements()
-            {
-                var Elements = new List<PredictionElement>();
-                int b = 0;
-                char[] cs = Phrase.ToCharArray();
-                for (int i = 1; i < cs.Count(); i++)
+                if (!char.IsLetter(cs[i]))
                 {
-                    if (!char.IsLetter(cs[i]))
+                    if (i > b && b < cs.Count())
+                    {
+                        string z = string.Empty;
+                        for (int j = b; j < i; j++)
+                        {
+                            z += cs[j];
+                        }
+                        Elements.Add(new PredictionElement(z));
+                    }
+                    b = i + 1;
+                }
+                else
+                {
+                    if (char.IsUpper(cs[i]) && !char.IsUpper(cs[i - 1]))
                     {
                         if (i > b && b < cs.Count())
                         {
@@ -36,87 +51,72 @@ namespace CC_Library
                             {
                                 z += cs[j];
                             }
-                            Elements.Add(new PredictionElement(z));
+                            //data.Add(z);
                         }
-                        b = i + 1;
-                    }
-                    else
-                    {
-                        if (char.IsUpper(cs[i]) && !char.IsUpper(cs[i - 1]))
-                        {
-                            if (i > b && b < cs.Count())
-                            {
-                                string z = string.Empty;
-                                for (int j = b; j < i; j++)
-                                {
-                                    z += cs[j];
-                                }
-                                //data.Add(z);
-                            }
-                            b = i;
-                        }
+                        b = i;
                     }
                 }
-                return Elements;
             }
+            return Elements;
         }
-        internal class PredictionElement
-        {
-            public string Word { get; }
-            internal List<PredOption> Options { get; set; }
+    }
+    internal class PredictionElement
+    {
+        public string Word { get; }
+        internal List<PredOption> Options { get; set; }
         
-            public PredictionElement(string w)
-            {
-                this.Word = w;
-                this.Options = new List<PredOption>();
-            }
-            public PredictionElement(XElement ele)
-            {
-                this.Word = ele.Attribute("WORD").Value;
-                this.Options = new List<PredOption>();
-                foreach(XElement e in ele.Elements("OPTION"))
-                {
-                    Options.Add(new PredOption(e));
-                }
-            }
-            public XElement CreateElement()
-            {
-                XElement ele = new XElement("ELEMENT");
-                ele.Add(new XAttribute("WORD", Word));
-                foreach(PredOption p in Options)
-                {
-                    ele.Add(p.CreateOption());
-                }
-                return ele;
-            }
-        }
-        internal class PredOption
+        public PredictionElement(string w)
         {
-            public string Name { get; }
-            public double Adjustment { get; set; }
-            public int Count { get; set; }
-            
-            public PredOption(string Word)
+            this.Word = w;
+            this.Options = new List<PredOption>();
+        }
+        public PredictionElement(XElement ele)
+        {
+            this.Word = ele.Attribute("WORD").Value;
+            this.Options = new List<PredOption>();
+            foreach(XElement e in ele.Elements("OPTION"))
             {
-                this.Name = Word;
-                this.Adjustment = 0;
-                this.Count = 1;
-            }
-            public PredOption(XElement ele)
-            {
-                this.Name = ele.Attribute("NAME").Value;
-                this.Adjustment = double.Parse(ele.Attribute("ADJUSTMENT").Value);
-                this.Count = int.Parse(ele.Attribute("QTY").Value);
-            }
-            public XElement CreateOption()
-            {
-                XElement ele = new XElement("OPTION");
-                ele.Add(new XAttribute("NAME", this.Name));
-                ele.Add(new XAttribute("ADJUSTMENT", this.Adjustment.ToString()));
-                ele.Add(new XAttribute("QTY", this.Count.ToString()));
-                return ele;
+                Options.Add(new PredOption(e));
             }
         }
+        public XElement CreateElement()
+        {
+            XElement ele = new XElement("ELEMENT");
+            ele.Add(new XAttribute("WORD", Word));
+            foreach(PredOption p in Options)
+            {
+                ele.Add(p.CreateOption());
+            }
+            return ele;
+        }
+    }
+    internal class PredOption
+    {
+        public string Name { get; }
+        public double Adjustment { get; set; }
+        public int Count { get; set; }
+            
+        public PredOption(string Word)
+        {
+            this.Name = Word;
+            this.Adjustment = 0;
+            this.Count = 1;
+        }
+        public PredOption(XElement ele)
+        {
+            this.Name = ele.Attribute("NAME").Value;
+            this.Adjustment = double.Parse(ele.Attribute("ADJUSTMENT").Value);
+            this.Count = int.Parse(ele.Attribute("QTY").Value);
+        }
+        public XElement CreateOption()
+        {
+            XElement ele = new XElement("OPTION");
+            ele.Add(new XAttribute("NAME", this.Name));
+            ele.Add(new XAttribute("ADJUSTMENT", this.Adjustment.ToString()));
+            ele.Add(new XAttribute("QTY", this.Count.ToString()));
+            return ele;
+        }
+    }
     public class TitleAnalysisPrediction
     {
         public delegate void TEST(string s);
