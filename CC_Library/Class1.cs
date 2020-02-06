0bce7446a22f25e6e52752c0b020b2dc0aa0569a
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-02-06 10:40:58 -0800

    Update Class1.cs

--- CC_Library/Class1.cs
+++ CC_Library/Class1.cs
@@ -93,19 +93,22 @@ namespace CC_Library
     internal class PredOption
     {
         public string Name { get; }
-        public double Adjustment { get; set; }
+        public float Adjustment { get; set; }
+        public float Weight { get; set; }
         public int Count { get; set; }
             
-        public PredOption(string Word)
+        public PredOption(string w)
         {
-            this.Name = Word;
+            this.Name = w;
             this.Adjustment = 0;
+            this.Weight = 1;
             this.Count = 1;
         }
         public PredOption(XElement ele)
         {
             this.Name = ele.Attribute("NAME").Value;
-            this.Adjustment = double.Parse(ele.Attribute("ADJUSTMENT").Value);
+            this.Adjustment = float.Parse(ele.Attribute("ADJUSTMENT").Value);
+            this.Weight = float.Parse(ele.Attribute("WEIGHT").Value);
             this.Count = int.Parse(ele.Attribute("QTY").Value);
         }
         public XElement CreateOption()
@@ -113,6 +116,7 @@ namespace CC_Library
             XElement ele = new XElement("OPTION");
             ele.Add(new XAttribute("NAME", this.Name));
             ele.Add(new XAttribute("ADJUSTMENT", this.Adjustment.ToString()));
+            ele.Add(new XAttribute("WEIGHT", this.Weight.ToString());
             ele.Add(new XAttribute("QTY", this.Count.ToString()));
             return ele;
         }
