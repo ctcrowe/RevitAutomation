0ed3860f76a8a12e241dc328e7e9b8de74434ce3
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-02-07 15:40:10 -0800

    Update _CMD_CC_GenPredictions.cs

--- CC_Library/CC_Predictions/_CMD_CC_GenPredictions.cs
+++ CC_Library/CC_Predictions/_CMD_CC_GenPredictions.cs
@@ -1,12 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+using System.Globalization;
 
 namespace CC_Library
 {
-    class CMD_CC_GenPredictions
+    public class GenPredictions
     {
+        private static string directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        private static string Dataset = directory + "\\CC_XMLData";
+        private static string FileName = directory + "\\CC_MFData.xml";
+        
+        public static void Run()
+        {
+            if(Directory.Exists(Dataset))
+            {
+                string[] files = Directory.GetFiles(folder);
+                foreach(string f in files)
+                {
+                    XDocument doc = XDocument.Load(f);
+                    if(doc.Root.Attribute("MFSection") != null)
+                    {
+                        
+                    }
+                }
+            }
+        }
     }
 }
