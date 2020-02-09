be0391d7f5157b700fa55440f4ced696d3cf6432
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-02-08 23:20:35 -0800

    Update CC_PredictionOption.cs

--- CC_Library/CC_Predictions/CC_PredictionOption.cs
+++ CC_Library/CC_Predictions/CC_PredictionOption.cs
@@ -5,21 +5,21 @@ namespace CC_Library
     internal class PredictionOption
     {
         public string Name { get; }
-        public double Adjustment { get; set; }
+        public double Ratio { get; set; }
         public double Weight { get; set; }
         public int Count { get; set; }
             
         public PredictionOption(string w)
         {
             this.Name = w;
-            this.Adjustment = 0;
+            this.Ratio = 0;
             this.Weight = 1;
             this.Count = 1;
         }
         public PredictionOption(XElement ele)
         {
             this.Name = ele.Attribute("NAME").Value;
-            this.Adjustment = double.Parse(ele.Attribute("ADJUSTMENT").Value);
+            this.Ratio = double.Parse(ele.Attribute("RATIO").Value);
             this.Weight = double.Parse(ele.Attribute("WEIGHT").Value);
             this.Count = int.Parse(ele.Attribute("QTY").Value);
         }
@@ -27,11 +27,15 @@ namespace CC_Library
         {
             XElement ele = new XElement("OPTION");
             ele.Add(new XAttribute("NAME", this.Name));
-            ele.Add(new XAttribute("ADJUSTMENT", this.Adjustment.ToString()));
+            ele.Add(new XAttribute("RATIO", this.Adjustment.ToString()));
             ele.Add(new XAttribute("WEIGHT", this.Weight.ToString()));
             ele.Add(new XAttribute("QTY", this.Count.ToString()));
             return ele;
         }
         public void AdjustCount() { Count++; }
+        public void SetRatio(int i)
+        {
+            this.Ratio = Count / i;
+        }
     }
 }
