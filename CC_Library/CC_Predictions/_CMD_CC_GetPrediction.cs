d12f9ef93f84b18383629ad9f0dc6cf1d2ff29a8
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-02-10 13:29:22 -0800

    Update _CMD_CC_GetPrediction.cs

--- CC_Library/CC_Predictions/_CMD_CC_GetPrediction.cs
+++ CC_Library/CC_Predictions/_CMD_CC_GetPrediction.cs
@@ -8,5 +8,29 @@ namespace CC_Library
 {
     class CMD_CC_GetPrediction
     {
+        public string Predict()
+        {
+            foreach(PredictionElement e in Elements)
+            {
+                foreach(PredictionOption o in e.Options)
+                {
+                    if(!this.Options.Any(x => x.Name == o.Name))
+                    {
+                        this.Options.Add(new Prediction(o.Name));
+                    }
+                    double val = o.Weight * o.Adjustment;
+                    this.Options.Where(x => x.Name == o.Name).First().Adjust(val);
+                }
+            }
+            double Max = 0;
+            foreach(Prediction p in this.Options)
+            {
+                if(p.Weight > Max)
+                    Max = p.Weight;
+            }
+            string s = Options.Where(x => x.Weight == Max).First().Name;
+            this.Prediction = s;
+            return s;
+        }
     }
 }
