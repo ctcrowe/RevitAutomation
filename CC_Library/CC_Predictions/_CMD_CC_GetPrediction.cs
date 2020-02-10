bdefc3b4f8e84d94bf0e5adc198b557985fdb188
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-02-10 13:31:35 -0800

    Update _CMD_CC_GetPrediction.cs

--- CC_Library/CC_Predictions/_CMD_CC_GetPrediction.cs
+++ CC_Library/CC_Predictions/_CMD_CC_GetPrediction.cs
@@ -6,6 +6,26 @@ using System.Threading.Tasks;
 
 namespace CC_Library
 {
+/*
+    public PredictionElement(string w)
+    {
+        this.Word = w;
+        this.Options = new List<PredOption>();
+    }
+    public PredOption(string w)
+    {
+        this.Name = w;
+        this.Adjustment = 0;
+        this.Weight = 1;
+        this.Count = 1;
+    }
+    public Prediction(string s)
+    {
+        this.Name = s;
+        this.Weight = 0;
+        this.Count = 0;
+    }
+*/
     class CMD_CC_GetPrediction
     {
         public string Predict()
