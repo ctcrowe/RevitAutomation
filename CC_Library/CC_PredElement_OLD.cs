aa51241da79ea49d47af90ab67d71b99cdf49520
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2020-02-11 09:55:35 -0800

    Update CC_PredElement_OLD.cs

--- CC_Library/CC_PredElement_OLD.cs
+++ CC_Library/CC_PredElement_OLD.cs
@@ -6,31 +6,40 @@ using System;
 
 namespace CC_Library
 {
-    public class PredElement
+    public class AdjustPredictions
     {
         private static string directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         private static string InputFile = directory + "\\CC_MFData.xml";
         private static string OutputFile = directory + "\\CC_MasterformatPredictor.xml";
 
-        public void AdjustPredictions(double[] Data, int Correct)
+        public static void Math(List<PredictionElement> pes)
         {
-            double MaxChange = (1 / (Math.Pow(PredictionNumber, 2) + 1));
-            double mv = Data.Max();
-            int Guess = Array.IndexOf(Data, mv);
-            if(Guess == Correct && mv > 0.75)
+            
+        }
+        public static void run()
+        {
+            if(File.Exists(InputFile))
             {
-                Predictions[Correct] += MaxChange / 2;
-            }
-            else
-            {
-                Predictions[Correct] += MaxChange;
-                for(int i = 0; i < Predictions.Count(); i++)
+                XDocument indoc = XDocument.Load(InputFile);
+                double MaxChange = (1 / (Math.Pow(PredictionNumber, 2) + 1));
+                double mv = Data.Max();
+                int Guess = Array.IndexOf(Data, mv);
+                if(Guess == Correct && mv > 0.75)
                 {
-                    if(i != Correct)
-                       Predictions[i] -= (MaxChange / PredictionCount);
+                    Predictions[Correct] += MaxChange / 2;
+                }
+                else
+                {
+                    Predictions[Correct] += MaxChange;
+                    for(int i = 0; i < Predictions.Count(); i++)
+                    {
+                        if(i != Correct)
+                           Predictions[i] -= (MaxChange / PredictionCount);
+                    }
+                }
+                PredictionNumber += 1;
                 }
             }
-            PredictionNumber += 1;
         }
     }
 }
