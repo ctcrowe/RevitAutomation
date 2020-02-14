9afe3d4159c0d1e3b7f7f7a2867a458b1bb6e278
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2020-02-14 14:15:15 -0800

    Update _CMD_CC_GenPredictions.cs

--- CC_Library/CC_Predictions/_CMD_CC_GenPredictions.cs
+++ CC_Library/CC_Predictions/_CMD_CC_GenPredictions.cs
@@ -10,12 +10,21 @@ namespace CC_Library
     public class GenPredictions
     {
         private static string directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        private static string FileName = directory + "\\CC_MFData.xml";
+        private static string Input = directory + "\\CC_MasterformatPredictor.xml";
         
         public static string Run(string s)
         {
-            PredictionPhrase p = new PredictionPhrase(s);
-            return AdjustPredictions.RunFormula(p.Elements);
+            List<string> x = s.SplitTitle();
+            List<PredictionElement> pes = new List<PredictionElement>();
+            XDocument doc = XDocument.Load(Input);
+            foreach(string y in x)
+            {
+                if(doc.Root.Elements().Any(x => x.Attribute("WORD").Value == y))
+                {
+                    pes.Add(new PredictionElement(doc.Root.Elements().Where(x => x.Attribute("WORD").Value == y).First());
+                }
+            }
+            return AdjustPredictions.RunFormula(pes);
         }
     }
 }
