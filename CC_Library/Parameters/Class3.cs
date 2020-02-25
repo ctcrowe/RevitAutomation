2a691069543c1a6ccc62bd45447da7dc3d3c2be3
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2020-02-25 15:55:06 -0800

    Update Class3.cs

--- CC_Library/Parameters/Class3.cs
+++ CC_Library/Parameters/Class3.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace CC_Library.Parameters
 {
+    public sealed class FireRatingParam : Param
+    {
+        public string = "FireProtection";
+        public string name = "FIRE RATING";
+        public int type = ParamType.Double;
+        public Guid ID;
+        public Boolean Vis; = true;
+        public string Description;
+        public Boolean UsrMod = true;
+        public Boolean Inst = false;
+        public Boolean Fixed = true
+    }
     class Class3
     {
+        /*
+        DOOR SCHEDULE
+        
+        Door Number
+        Fire Rating
+        HW Set
+        Door Access Control
+        Type
+        Opening Width
+        Opening Height
+        Door Thickness
+        Door Material
+        Door Finish
+        Frame Material
+        Frame Push
+        Frame Pull
+        Jamb Detail
+        Head Detail
+        Sill Detail
+        Remarks
+        
+        WALL SCHEDULE
+        Wall Type
+        Stud Size
+        Approximate Overall Dimension
+        Fire Rating
+        UL Number
+        Insulation Type
+        Notes
+        */
     }
 }
