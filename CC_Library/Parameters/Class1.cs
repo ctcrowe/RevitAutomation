4542c6ca8bcdef544293b4cd3e57e8554349a477
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2020-02-27 10:10:56 -0800

    Update Class1.cs

--- CC_Library/Parameters/Class1.cs
+++ CC_Library/Parameters/Class1.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace CC_Library.Parameters
 {
-    class Class1
+    public sealed class Width : Param
     {
+        public string Category = "Dimensions";
+        public string name = "oW";
+        public int type = ParamType.Length;
+        public Guid ID = new Guid("7c53c6ed-278f-4036-b5c7-eac8437ab28a");
+        public Boolean Vis = true;
+        public string Description = "USED TO DEFINE THE OVERALL OUTSIDE MAXIMUM DIMENSIONS OF AN OBJECT, REFERENCING LEFT AND RIGHT EXTREMES";
+        public Boolean UsrMod = true;
+        public Boolean Inst = false;
+        public Boolean Fixed = true;
     }
 }
