69d4f18c39e6775bc4d0d287342316acba0d3f9d
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2020-02-28 08:52:24 -0800

    Update RoomCategoryParam.cs

--- CC_Library/Parameters/Library/RoomCategoryParam.cs
+++ CC_Library/Parameters/Library/RoomCategoryParam.cs
@@ -4,14 +4,15 @@ namespace CC_Library.Parameters
 {
     public sealed class RoomOccupancy : Param
     {
-        public string Category = "Rooms";
-        public string name = "ROOM CATEGORY";
-        public int type = ParamType.String;
-        public Guid ID = new Guid("819cfd72-96c9-4fff-b303-4765879f9747");
-        public Boolean Vis = true;
-        public string Description = "A REFERENCE TO THE OCCUPANCY OF THE ROOM.";
-        public Boolean UsrMod = true;
-        public Boolean Inst = true;
-        public Boolean Fixed = true;
+        public readonly string Category = "Rooms";
+        public readonly string name = "ROOM CATEGORY";
+        public readonly int type = ParamType.String;
+        public readonly Guid ID = new Guid("819cfd72-96c9-4fff-b303-4765879f9747");
+        public resadonly Boolean Vis = true;
+        public readomly string Description = "A REFERENCE TO THE OCCUPANCY OF THE ROOM.";
+        public readonly Boolean UsrMod = true;
+        public readonly Boolean Inst = true;
+        public readonly Boolean Fixed = true;
+        public string Value { get; set; }
     }
 }
