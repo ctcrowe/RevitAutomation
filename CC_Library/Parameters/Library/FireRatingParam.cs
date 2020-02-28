b203eaefa42e70e0282e09ed0822634f62275b98
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2020-02-28 08:39:21 -0800

    Update FireRatingParam.cs

--- CC_Library/Parameters/Library/FireRatingParam.cs
+++ CC_Library/Parameters/Library/FireRatingParam.cs
@@ -4,14 +4,14 @@ namespace CC_Library.Parameters
 {
     public sealed class FireRating : Param
     {
-        public string Category = "FireProtection";
-        public string name = "FIRE RATING";
-        public int type = ParamType.String;
-        public Guid ID = new Guid("4333b0f4-1652-4d7a-8060-2044707e0815");
-        public Boolean Vis = true;
-        public string Description = "Fire Ratings foor Doors, Windows, and Walls.";
-        public Boolean UsrMod = true;
-        public Boolean Inst = false;
-        public Boolean Fixed = true;
+        public static readonly string Category = "FireProtection";
+        public static readonly string name = "FIRE RATING";
+        public static readonly int type = ParamType.String;
+        public static readonly Guid ID = new Guid("4333b0f4-1652-4d7a-8060-2044707e0815");
+        public static readonly Boolean Vis = true;
+        public static readonly string Description = "Fire Ratings foor Doors, Windows, and Walls.";
+        public static readonly Boolean UsrMod = true;
+        public static readonly Boolean Inst = false;
+        public static readonly Boolean Fixed = true;
     }
 }
