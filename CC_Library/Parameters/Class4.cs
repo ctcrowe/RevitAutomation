8c4b27171269949adea2db63b5d8d458954180ec
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2020-03-02 11:23:44 -0800

    Update Class4.cs

--- CC_Library/Parameters/Class4.cs
+++ CC_Library/Parameters/Class4.cs
@@ -7,5 +7,6 @@ namespace CC_Library.Parameters
         public const int Family = 1;
         public const int Both = 2;
         public const int Space = 3;
+        public const int Wall = 4;
     }
 }
