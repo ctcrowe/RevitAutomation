4c426e2144a157df84c2b5951242cdf4f28f1c76
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2020-03-02 10:33:33 -0800

    Update Class4.cs

--- CC_Library/Parameters/Class4.cs
+++ CC_Library/Parameters/Class4.cs
@@ -1,35 +1,10 @@
-using System;
-using System.Linq;
-using System.IO;
-using System.Reflection;
-
+  
 namespace CC_Library.Parameters
 {
-
-    public class DataCouplet
+    public static class ParamLocation
     {
-        Param P { get; set;}
-        string V { get; set;}
-        
-        public DataCouplet(Param p, string v)
-        {
-            this.P = p;
-            this.V = v;
-        }
-        public DataCouplet(Param p)
-        {
-            this.P = p;
-            this.V = string.Empty;
-        }
-    }
-    public class CC_Element
-    {
-        string ID { get; }
-        DataCouplet[] Dataset { get; set; }
-        public CC_Element(DataCouplet[] data)
-        {
-            this.ID = Guid.NewGuid().ToString();
-            this.Dataset = data;
-        }
+        public const int Project = 0;
+        public const int Family = 1;
+        public const int Both = 2;
     }
 }
