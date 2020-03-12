92d4dc1aab5864bd400d90776e21b157e7280dc5
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2020-03-11 22:08:37 -0700

    Adding classes for experimenting

---
+++ CC_Library/Predictions2.0/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_Library.Predictions2._0
+{
+    class Class1
+    {
+    }
+}
