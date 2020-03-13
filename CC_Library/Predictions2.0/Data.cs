12208d0fafd8c54d9f1697486362752e0cb84780
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2020-03-13 09:28:40 -0700

    Update Data.cs

--- CC_Library/Predictions2.0/Data.cs
+++ CC_Library/Predictions2.0/Data.cs
@@ -1,4 +1,4 @@
-﻿namespace CC_Library.Predictions
+namespace CC_Library.Predictions
 {
     internal class Data
     {
@@ -8,6 +8,11 @@
         {
             this.Phrase = phrase;
             this.Dataset = new int[20];
+            Random rnd = new Random();
+            for(int i = 0; i < Dataset.Count(); i++)
+            {
+                Dataset[i] = rnd.Next(1, 100);
+            }
         }
         public void SetValue(int i, int value)
         {
