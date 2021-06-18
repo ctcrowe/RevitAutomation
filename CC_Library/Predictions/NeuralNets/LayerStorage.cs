ba359a6262bb18a84efd2b0330b3d6f2dc434c60
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-06-18 09:11:49 -0700

    Update LayerStorage.cs

--- CC_Library/Predictions/NeuralNets/LayerStorage.cs
+++ CC_Library/Predictions/NeuralNets/LayerStorage.cs
@@ -9,10 +9,10 @@ namespace CC_Library.Predictions
     internal class NetworkMem
     {
         public List<LayerMem> Layers { get; set; }
-        public NetworkMem(NeuralNetwork net, int RunSize)
+        public NetworkMem(NeuralNetwork net)
         {
             this.Layers = new List<LayerMem>();
-            Parallel.For(0, net.Layers.Count, i => Layers[i] = new LayerMem(net.Layers[i], RunSize));
+            Parallel.For(0, net.Layers.Count, i => Layers[i] = new LayerMem(net.Layers[i]));
         }
     }
     internal class LayerMem
@@ -40,21 +40,10 @@ namespace CC_Library.Predictions
                 }
             }
         }
-        /*
-        public void AddDerivative(Layer l)
+        public void Reset()
         {
-            if (l.Weights.GetLength(0) == Weights.GetLength(0) && l.Weights.GetLength(1) == Weights.GetLength(1))
-            {
-                for (int i = 0; i < Weights.GetLength(0); i++)
-                {
-                    DeltaB[i] += l.DeltaB[i];
-                    for (int j = 0; j < Weights.GetLength(1); j++)
-                    {
-                        DeltaW[i, j] += l.DeltaW[i, j];
-                    }
-                }
-            }
+            DeltaB = new double[layer.Biases.Count()];
+            DeltaW = new double[l.Weights.GetLength(0), l.Weights.GetLength(1)];
         }
-        */
     }
 }
