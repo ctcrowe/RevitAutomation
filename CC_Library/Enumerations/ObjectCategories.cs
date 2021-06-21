581494f0daa7b6806d1dc8312dcc7bd4e6efc326
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-06-21 10:27:10 -0700

    Update ObjectCategories.cs

--- CC_Library/Enumerations/ObjectCategories.cs
+++ CC_Library/Enumerations/ObjectCategories.cs
@@ -2,22 +2,29 @@
 {
     public enum ObjectCategory
     {
+    //Door and Window
         Swing,
         Head,
         Jamb,
         Sill,
+        Panel,
+        Glazing,
+    //Cabinetry
         Box,
         Outline,
         Counter,
         Base,
         Pull,
-        Panel,
-        Glazing,
         Details,
+    //Details
         Det_Beyond,
         Det_03_XFine,
+        Det_03_XFine_Dash,
         Det_04_Fine,
+        Det_04_Fine_Dash,
         Det_05_Medium,
-        Det_06_Heavy
+        Det_05_Medium_Dash,
+        Det_06_Heavy,
+        Det_06_Heavy_Dash
     }
 }
