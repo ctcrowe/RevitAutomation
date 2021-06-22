2e331ee5230858be68c18251aa3c9184621d1200
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-06-22 10:01:28 -0700

    Update Datatypes.cs

--- CC_Library/Enumerations/Datatypes/Datatypes.cs
+++ CC_Library/Enumerations/Datatypes/Datatypes.cs
@@ -1,29 +1,5 @@
 ﻿namespace CC_Library.Datatypes
 {
-    /*
-     * Datatype number will reference the following
-     * 1000s) references the number of values in the array representing the feature in space.
-     *          An n dimensional array will represent it.
-     *          Each value of this array does NOT have to be linear when compared against another
-     *              EX: 1st might reference dimension 3, 2nd dimension 4, etc.
-     *          Every array will be smaller than the Dictionary Array, which will contain 7 numbers.
-     *          This allows relationships to develop between Datatypes that are not exclusively interrelated.
-     *          Some datatypes will have stronger relations to each other than others.
-     */
-
-        /// <summary>
-        /// New Datatypes:
-        /// 7 Number Format
-        /// Second Number Represents Number of Layers
-        /// Third Number Represents Layer Size
-        /// Fourth and Fifth Numbers represent the last layer size (Accept in the Dictionary)
-        /// Last Two Numbers are to give it a unique ID
-        /// All Entry and Exit information will be contained within 8 values
-
-        /// A new datatype is created! Softmax Functions will begin with a "1"
-        /// Other datatypes will begin with a 2
-        /// if the system is a softmax system, the last layer needs to be the size of the options.
-
     public enum Datatype
     {
         Dictionary,
