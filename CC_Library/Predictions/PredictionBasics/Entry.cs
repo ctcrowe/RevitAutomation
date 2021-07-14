20a04f70939d88a68076caad65ab2ce0a64dd6fe
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-07-14 08:46:32 -0700

    Update Entry.cs

--- CC_Library/Predictions/PredictionBasics/Entry.cs
+++ CC_Library/Predictions/PredictionBasics/Entry.cs
@@ -1,12 +1,24 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using CC_Library.Datatypes;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace CC_Library.Predictions
 {
+    [Serializable]
     internal class Entry
     {
-        public string[] Keys;
-        public string[] Values;
-        public bool correct;
+        public string Datatype;
+        public string TextInput;
+        public double[] ValInput;
+        public double[] ImgInput;
+        public double[] DesiredOutput;
+        public Entry(Datatype dt)
+        {
+            this.Datatype = dt.ToString();
+        }
     }
 }
