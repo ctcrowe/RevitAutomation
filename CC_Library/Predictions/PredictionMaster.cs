760e2c08d2853fc689e853b3b4dc1aacfabdad86
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-07-16 11:39:15 -0700

    Update PredictionMaster.cs

--- CC_Library/Predictions/PredictionMaster.cs
+++ CC_Library/Predictions/PredictionMaster.cs
@@ -24,5 +24,20 @@ namespace CC_Library.Predictions
             var output = Network.Predict(entry);
             return output.ToList().IndexOf(output.Max());
         }
+        public static void PropogateSingle(this Datatype dt, int correct, WriteToCMDLine write, string s = null, double[] other = null, double[] img = null)
+        {
+            Sample entry = new Sample(dt);
+            if(s != null) { entry.TextInput = s; }
+            if(other != null) { entry.ValInput = other; }
+            if (img != null) { entry.ImgInput = img; }
+
+            var type = typeof(INetworkPredUpdater);
+            Assembly a = type.Assembly;
+            var NetType = a.GetTypes().Where(x => type.IsAssignableFrom(x)).Where(y => (y as INetworkPredUpdater).datatype == dt).First();
+            INetworkPredUpdater Network = NetType as INetworkPredUpdater;
+            entry.DesiredOutput = new double[Network.Network.Layers.Last().Biases.Count()];
+            entry.DesiredOutput[correct] = 1;
+            Network.Propogate(entry, write);
+        }
     }
 }
