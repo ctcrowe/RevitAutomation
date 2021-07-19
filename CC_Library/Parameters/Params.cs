04e79e36f09177009bd275f24ae92871e074d0e6
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-07-19 09:45:11 -0700

    Update Params.cs

--- CC_Library/Parameters/Params.cs
+++ CC_Library/Parameters/Params.cs
@@ -4,13 +4,6 @@ namespace CC_Library.Parameters
 {
     public static class Params
     {
-        public static readonly Param ViewID = new Param
-            ("ViewID",
-            new Guid("bc77260bddda4eaa835fe21a4e95dd50"),
-            Subcategory.View,
-            ParamType.Text,
-            true,
-            false);
         public static readonly Param Masterformat = new Param
             ("MF Division",
             new Guid("aedbf7d2d73c44048cf7ed26f3231d25"),
@@ -18,13 +11,6 @@ namespace CC_Library.Parameters
             ParamType.Int,
             false,
             true);
-        public static readonly Param Masterformat_Material = new Param
-            ("MF Division Material",
-            new Guid("c0409866752d448b9ae10c03942ba603"),
-            Subcategory.Materials,
-            ParamType.Int,
-            true,
-            false);
         public static readonly Param Finish = new Param
             ("Finish Material",
             new Guid("4848e7f4ee234755b9892deb5eef17b6"),
