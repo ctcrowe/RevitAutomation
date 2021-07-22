7626cd6f7d2ab52331c09a061de9d4c9821298f1
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-07-22 16:13:51 -0700

    Update OpenFile.cs

--- CC_Library/OpenFile.cs
+++ CC_Library/OpenFile.cs
@@ -6,9 +6,9 @@ using CC_Library.Datatypes;
 
 namespace CC_Library
 {
-    public static class OpenFile
+    public static class Files
     {
-        public static string Run()
+        public static string Open()
         {
             string filepath = null;
             OpenFileDialog ofd = new OpenFileDialog()
@@ -21,6 +21,20 @@ namespace CC_Library
                 filepath = ofd.FileName;
             }
             return filepath;
+        }
+        public static string GetFile()
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                return saveFileDialog1.FileName;
+            }
+            return null;
         }
     }
 }
