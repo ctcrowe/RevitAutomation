c146986a2036acdf46591a1e219f13fcde58c788
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-07-22 16:02:20 -0700

    Update CC_CmdLibrary.cs

--- CC_Library/CC_CmdLibrary.cs
+++ CC_Library/CC_CmdLibrary.cs
@@ -12,6 +12,27 @@ namespace CC_Library
 {
     public static class CMDLibrary
     {
+        public static void OutputError(this Exception ex)
+        {
+            string f = "Error.txt";
+            string filepath = f.GetMyDocs();
+
+            using (StreamWriter writer = new StreamWriter(filepath, true))
+            {
+                writer.WriteLine("-----------------------------------------------------------------------------");
+                writer.WriteLine("Date : " + DateTime.Now.ToString());
+                writer.WriteLine();
+
+                while (ex != null)
+                {
+                    writer.WriteLine(ex.GetType().FullName);
+                    writer.WriteLine("Message : " + ex.Message);
+                    writer.WriteLine("StackTrace : " + ex.StackTrace);
+
+                    ex = ex.InnerException;
+                }
+            }
+        }
         public static double[] GetRank(this double[,] D, int l)
         {
             double[] r = new double[D.GetLength(1)];
