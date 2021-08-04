4cf8886966ecf9b29b1a16d7056d7efbd351741a
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-08-04 09:49:44 -0700

    Update LineStyleNetworkUpdater.cs

--- CC_Events/Events/Subcategories/LineStyleNetworkUpdater.cs
+++ CC_Events/Events/Subcategories/LineStyleNetworkUpdater.cs
@@ -30,7 +30,8 @@ namespace CC_Plugin
         {
             LineStyleNetworkUpdater updater = new LineStyleNetworkUpdater(id);
             UpdaterRegistry.RegisterUpdater(updater, true);
-            ElementId pid = new ElementId(BuiltInParameter.CLINE_SUBCATEGORY);
+            //ElementId pid = new ElementId(BuiltInParameter.CLINE_SUBCATEGORY);
+            ElementId pid = new ElementId(BuiltInParameter.FAMILY_ELEM_SUBCATEGORY);
             UpdaterRegistry.AddTrigger(updater.GetUpdaterId(),
                 new ElementClassFilter(typeof(CurveElement)),
                 Element.GetChangeTypeParameter(pid));
