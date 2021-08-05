acd7344bfd4aeace5ee65dfeea6b77d7078d9802
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-08-05 10:30:01 -0700

    Update StyleNetwork.cs

--- CC_Library/Predictions/Prediction - Object Style/StyleNetwork.cs
+++ CC_Library/Predictions/Prediction - Object Style/StyleNetwork.cs
@@ -76,7 +76,7 @@ namespace CC_Library.Predictions
                         AlphaMem am = new AlphaMem(s.TextInput.ToCharArray());
                         s.TextOutput = a.Forward(s.TextInput, ctxt, am, write);
                         var F = Forward(s, write);
-                        COutput.OutputError(CategoricalCrossEntropy.Forward(F.Last()));
+                        s.OutputError(CategoricalCrossEntropy.Forward(F.Last()));
                     
                         var DValues = Backward(s, F, ObjMem, WriteNull);
                         a.Backward(s.TextInput, DValues, ctxt, am, AlphaMem, CtxtMem, write);
