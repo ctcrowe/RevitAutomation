e9b5f66b416d226a08000158a0050bc23adca9d1
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-08-09 09:37:30 -0700

    Update CC_CmdLibrary.cs

--- CC_Library/CC_CmdLibrary.cs
+++ CC_Library/CC_CmdLibrary.cs
@@ -14,6 +14,22 @@ namespace CC_Library
 {
     public static class CMDLibrary
     {
+        public static void WriteToBinaryFile<T>(string filePath, T objectToWrite, bool append = false)
+        {
+            using (Stream stream = File.Create(filePath))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, objectToWrite);
+            }
+        }
+        public static T ReadFromBinaryFile<T>(this string filePath)
+        {
+            using (Stream stream = File.Open(filePath, FileMode.Open))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(stream);
+            }
+        }
         public static double[] Clipped(this double[] X)
         {
             double[] result = new double[X.Count()];
