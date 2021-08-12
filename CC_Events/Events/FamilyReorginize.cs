c12bc59826a7e0e120c85f0e0d9f2e5d07fa07b6
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-08-12 16:31:58 -0700

    Update FamilyReorginize.cs

--- CC_Events/Events/FamilyReorginize.cs
+++ CC_Events/Events/FamilyReorginize.cs
@@ -57,8 +57,8 @@ namespace CC_Plugin.Events
                 File.Delete(f);
             }
             foeach(var f in fnames)
-                               {
-                               }
+            {
+            }
         }
     }
 }
