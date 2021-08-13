1d5afc4a202aba0f86ec661493ed12f91d586e6c
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-08-13 10:27:52 -0700

    Update CC_SaveFamily.cs

--- CC_Events/Events/Masterformat/CC_SaveFamily.cs
+++ CC_Events/Events/Masterformat/CC_SaveFamily.cs
@@ -9,9 +9,32 @@ namespace CC_Plugin
 {
     internal static class SaveFamilyClass
     {
-        public static void SaveFamily(this string filename, string prefix = "test")
+        public static void SaveFamily(this string fp, string prefix = "test")
         {
+            string f = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            string folder = f + "\\CC_Families";
             
+            Sample s = new Sample(CC_Library.Datatypes.Datatype.Masterformat);
+            s.TextInput = fp.Split('\\').Last().Split('.').First();
+
+            MasterformatNetwork net = new MasterformatNetwork();
+            var div = net.Predict(s);
+            string Division = "Division " + div.ToList().IndexOf(div.Max());
+            string SubDir = folder + "\\" + Division;
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+            if (!Directory.Exists(SubDir))
+                Directory.CreateDirectory(SubDir);
+            if(!fp.Split('\\').Last().StartsWith(ftype + "_"))
+            {
+                string nf = SubDir + "\\" + ftype + "_" + fp.Split('\\').Last().Split('.').First() + ".rfa";
+                File.Copy(fp, nf, true);
+            }
+            else
+            {
+                string nf = SubDir + "\\" + fp.Split('\\').Last().Split('.').First() + ".rfa";
+                File.Copy(fp, nf, true);
+             }
         }
         public static void Main(Document doc)
         {
