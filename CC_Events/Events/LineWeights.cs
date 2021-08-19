c0446bfaa50aee78bb9da4d17d67c388bcd95533
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-08-19 10:50:37 -0700

    Update LineWeights.cs

--- CC_Events/Events/LineWeights.cs
+++ CC_Events/Events/LineWeights.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.DB;
 
 namespace CC_Plugin.Events
 {
-    class LineWeights
+    public class LineWeights
     {
+        public static void Learn(Document doc)
+        {
+            View v = doc.ActiveView;
+            
+        }
     }
 }
