a103876ceec87bef2dce3b5a1e74b73098a945bf
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-08-20 16:23:19 -0700

    Update LineWeights.cs

--- CC_Events/Events/LineWeights.cs
+++ CC_Events/Events/LineWeights.cs
@@ -23,23 +23,27 @@ namespace CC_Plugin.Events
                     {
                         foreach (Category cs in c.SubCategories)
                         {
-                            var ovrride = v.GetCategoryOverrides(cs.Id);
-                            string name1 = cs.Name;
-                            string name2 = v.Title;
-                            if(cs.GetLineWeight(GraphicsStyleType.Cut) != null)
+                            try
                             {
-                                int CutLineWeight = ovrride.CutLineWeight == OverrideGraphicsSettings.InvalidPenNumner?
-                                    (int)cs.GetLineWeight(GraphicsStyleType.Cut) :
-                                    ovrride.CutLineWeight;
-                                Datatype.CutLineWeight.PropogateSingle(CutLineWeight, new WriteToCMDLine(WriteNull), name1, name2);
+                                var ovrride = v.GetCategoryOverrides(cs.Id);
+                                string name1 = cs.Name;
+                                string name2 = v.Title;
+                                if(cs.GetLineWeight(GraphicsStyleType.Cut) != null)
+                                {
+                                    int CutLineWeight = ovrride.CutLineWeight == OverrideGraphicsSettings.InvalidPenNumner?
+                                        (int)cs.GetLineWeight(GraphicsStyleType.Cut) :
+                                        ovrride.CutLineWeight;
+                                    Datatype.CutLineWeight.PropogateSingle(CutLineWeight, new WriteToCMDLine(WriteNull), name1, name2);
+                                }
+                                if(cs.GetLineWeight(GraphicsStyleType.Projection) != null)
+                                {
+                                    int ProjectedLineWeight = ovrride.ProjectionLineWeight ==OverrideGraphicSettings.InvalidPenNumber?
+                                        (int) cs.GetLineWeight(GraphicStyleType.Projection):
+                                        ovrride.ProjectionLineWeight;
+                                    Datatype.ProjectedLineWeight.PropogateSingle(ProjectedLineWeight, new WriteToCMDLine(WriteNull), name1, name2);
+                                }
                             }
-                            if(cs.GetLineWeight(GraphicsStyleType.Projection) != null)
-                            {
-                                int ProjectedLineWeight = ovrride.ProjectionLineWeight ==OverrideGraphicSettings.InvalidPenNumber?
-                                    (int) cs.GetLineWeight(GraphicStyleType.Projection):
-                                    ovrride.ProjectionLineWeight;
-                                Datatype.ProjectedLineWeight.PropogateSingle(ProjectedLineWeight, new WriteToCMDLine(WriteNull), name1, name2);
-                            }
+                            catch(Exception e) {e.OutputError();}
                         }
                     }
                 }
