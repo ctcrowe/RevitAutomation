f09b0306276aad3b4dab11b832d3bb2a60b2f59a
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-08-25 11:13:19 -0700

    Update Accuracy.cs

--- CC_Library/Predictions/PredictionBasics/Accuracy.cs
+++ CC_Library/Predictions/PredictionBasics/Accuracy.cs
@@ -6,89 +6,48 @@ namespace CC_Library.Predictions
     public class Accuracy
     {
         public double Acc;
-        private string[] inputs;
         private double[] Distance;
         private int[] predicted;
         private int[] actual;
         private int[] Correct;
         private double[] Error;
-        private double StoredError;
-        private double StartingError;
-        private int Completion;
 
-        public Accuracy(string[] Lines)
+        public Accuracy(Sample[] s)
         {
-            int Count = Lines.Count();
+            int Count = s.Count();
 
             Acc = 0;
-            Completion = 0;
-            inputs = new string[Count];
             Distance = new double[Count];
             Correct = new int[Count];
             actual = new int[Count];
             predicted = new int[Count];
             Error = new double[Count];
-            StoredError = double.MaxValue;
-            StartingError = double.MaxValue;
-
-            Parallel.For(0, Count, i => actual[i] = int.Parse(Lines[i].Split(',').Last()));
         }
 
         public void Add(string input, int numb, double val, double error, int pred)
         {
-            inputs[numb] = input;
             predicted[numb] = pred;
             if (pred == actual[numb]) Correct[numb] = 1;
             else Correct[numb] = 0;
             Distance[numb] = val;
             Error[numb] = error;
-            Completion++;
         }
         public string[] Get()
         {
             string[] s = new string[13];
             Acc = 1.0 * Correct.Sum() / Correct.Count();
-            int MinDist = Distance.ToList().IndexOf(Distance.Min());
-            int MaxDist = Distance.ToList().IndexOf(Distance.Max());
-            int MinError = Error.ToList().IndexOf(Error.Min());
-            int MaxError = Error.ToList().IndexOf(Error.Max());
-
-            s[0] = "Min Confidence : " + Distance.Min() + " : " + inputs[MinDist] + " : " + predicted[MinDist] + " : " + actual[MinDist];
-            s[1] = "Max Confidence : " + Distance.Max() + " : " + inputs[MaxDist] + " : " + predicted[MaxDist] + " : " + actual[MaxDist];
+            s[0] = "Min Confidence : " + Distance.Min();
+            s[1] = "Max Confidence : " + Distance.Max();
             s[2] = "Average Confidence : " + (Distance.Sum() / Distance.Count());
             s[3] = "";
-            s[4] = "Min Error : " + Error.Min() + " : " + inputs[MinError] + " : " + predicted[MinError] + " : " + actual[MinError];
-            s[5] = "Max Error : " + Error.Max() + " : " + inputs[MaxError] + " : " + predicted[MaxError] + " : " + actual[MaxError];
+            s[4] = "Min Error : " + Error.Min() + " : " + inputs[MinError];
+            s[5] = "Max Error : " + Error.Max() + " : " + inputs[MaxError];
             s[6] = "Average Error : " + (Error.Sum() / Error.Count());
-            s[7] = "Stored Error : " + StoredError;
-            s[8] = "Starting Error : " + StartingError;
             s[9] = "";
             s[10] = "Accuracy : " + Correct.Sum() + " / " + Correct.Count() + " = " + Acc;
-            s[11] = "Percent Complete : " + Completion * 1.0 / inputs.Count();
             s[12] = "";
 
             return s;
-        }
-        public void Show(WriteToCMDLine write)
-        {
-            Acc = 1.0 * Correct.Sum() / Correct.Count();
-            int MinDist = Distance.ToList().IndexOf(Distance.Min());
-            int MaxDist = Distance.ToList().IndexOf(Distance.Max());
-            int MinError = Error.ToList().IndexOf(Error.Min());
-            int MaxError = Error.ToList().IndexOf(Error.Max());
-
-            write("Min Confidence : " + Distance.Min() + " : " + inputs[MinDist] + " : " + predicted[MinDist] + " : " + actual[MinDist]);
-            write("Max Confidence : " + Distance.Max() + " : " + inputs[MaxDist] + " : " + predicted[MaxDist] + " : " + actual[MaxDist]);
-            write("Average Confidence : " + (Distance.Sum() / Distance.Count()));
-            write("");
-            write("Min Error : " + Error.Min() + " : " + inputs[MinError] + " : " + predicted[MinError] + " : " + actual[MinError]);
-            write("Max Error : " + Error.Max() + " : " + inputs[MaxError] + " : " + predicted[MaxError] + " : " + actual[MaxError]);
-            write("Average Error : " + (Error.Sum() / Error.Count()));
-            write("Stored Error : " + StoredError);
-            write("Starting Error : " + StartingError);
-            write("");
-            write("Accuracy : " + Correct.Sum() + " / " + Correct.Count() + " = " + Acc);
-            write("Percent Complete : " + Completion * 1.0 / inputs.Count());
         }
         public bool CheckError()
         {
@@ -107,10 +66,6 @@ namespace CC_Library.Predictions
         public void SetAcc()
         {
             Acc = 1.0 * Correct.Sum() / Correct.Count();
-        }
-        public void Reset()
-        {
-            Completion = 0;
         }
         public void SetStartingError()
         {
