bf362e8a19eb1d65908ab5e6a5356e11075b0bc9
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-08-25 11:20:28 -0700

    Update Accuracy.cs

--- CC_Library/Predictions/PredictionBasics/Accuracy.cs
+++ CC_Library/Predictions/PredictionBasics/Accuracy.cs
@@ -6,9 +6,6 @@ namespace CC_Library.Predictions
     public class Accuracy
     {
         public double Acc;
-        private double[] Distance;
-        private int[] predicted;
-        private int[] actual;
         private int[] Correct;
         private double[] Error;
 
@@ -17,29 +14,21 @@ namespace CC_Library.Predictions
             int Count = s.Count();
 
             Acc = 0;
-            Distance = new double[Count];
             Correct = new int[Count];
-            actual = new int[Count];
-            predicted = new int[Count];
             Error = new double[Count];
         }
 
-        public void Add(string input, int numb, double val, double error, int pred)
+        public void Add(int numb, double error, int pred, int actual)
         {
             predicted[numb] = pred;
-            if (pred == actual[numb]) Correct[numb] = 1;
+            if (pred == actual) Correct[numb] = 1;
             else Correct[numb] = 0;
-            Distance[numb] = val;
             Error[numb] = error;
         }
         public string[] Get()
         {
             string[] s = new string[13];
             Acc = 1.0 * Correct.Sum() / Correct.Count();
-            s[0] = "Min Confidence : " + Distance.Min();
-            s[1] = "Max Confidence : " + Distance.Max();
-            s[2] = "Average Confidence : " + (Distance.Sum() / Distance.Count());
-            s[3] = "";
             s[4] = "Min Error : " + Error.Min() + " : " + inputs[MinError];
             s[5] = "Max Error : " + Error.Max() + " : " + inputs[MaxError];
             s[6] = "Average Error : " + (Error.Sum() / Error.Count());
