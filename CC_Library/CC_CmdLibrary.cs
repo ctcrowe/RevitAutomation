8943a2c1861085467260df260a91dde4ee323833
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-08-31 12:09:07 -0700

    Update CC_CmdLibrary.cs

--- CC_Library/CC_CmdLibrary.cs
+++ CC_Library/CC_CmdLibrary.cs
@@ -47,18 +47,8 @@ namespace CC_Library
         public static double[] Clipped(this double[] X)
         {
             double[] result = new double[X.Count()];
-            for(int i = 0; i < X.Count(); i++)
-            {
-                if (X[i] <= 0)
-                    result[i] = 1e-7;
-                else
-                {
-                    if (X[i] >= 1)
-                        result[i] = 1 - (1e-7);
-                    else
-                        result[i] = X[i];
-                }
-            }
+            Parallel.For(0, X.Count(), i =>
+                result[i] = X[i] <= 0? 1e-7 : X[i] >= 1? 1 - (1e-7) : X[i];
             return result;
         }
         public static double[,] DiagFlat(this double[] X)
