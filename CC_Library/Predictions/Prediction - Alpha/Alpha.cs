dea3548b1d3f7e43be4cd234528c03d3f5bbe1e3
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-09-02 09:11:12 -0700

    Update Alpha.cs

--- CC_Library/Predictions/Prediction - Alpha/Alpha.cs
+++ CC_Library/Predictions/Prediction - Alpha/Alpha.cs
@@ -23,7 +23,14 @@ namespace CC_Library.Predictions
 {
     internal class CharSet
     {
-        private static HashTable Chars = new HashTable() {'A' = 0, 'B' = 1};
+        public const int CharCount = 38;
+        private static char[] Chars = new char[38] {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G',
+            'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V', 'W', 'X', 'Y', 'Z', '0', '1',
+            '2', '3', '4', '5', '6', '7', '8',
+            '9', ' ', '_'};
     }
     internal class Alpha
     {
