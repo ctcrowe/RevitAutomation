270c115339e92c02b0a8af5926f81fd729f6da13
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-09-17 09:07:03 -0700

    Update BuySell.cs

--- Trader/BuySell.cs
+++ Trader/BuySell.cs
@@ -6,5 +6,63 @@ namespace Trader
 {
     class BuySell
     {
+        private static string API_KEY = "PK2CPPF4DJ29SX61712T";
+
+        private static string API_SECRET = "0XJpuQJ5QamtvrdlMsjxFj3YFPQ2Kqp3yNh9PnVx";
+
+        public static async Task Main()
+        {
+
+            Console.WriteLine("Test");
+            // First, open the API connection
+            var client = Alpaca.Markets.Environments.Paper
+                .GetAlpacaDataClient(new SecretKey(API_KEY, API_SECRET));
+
+            Console.WriteLine("Test");
+
+            var into = DateTime.Now;
+            into = into.AddHours(-1);
+            var from = into.AddDays(-3);
+
+            Console.WriteLine("Test");
+            try
+            {
+                var aaplbars = await client.ListHistoricalBarsAsync(new HistoricalBarsRequest("AAPL", from, into, BarTimeFrame.Hour));
+                var qqqbars = await client.ListHistoricalBarsAsync(new HistoricalBarsRequest("QQQ", from, into, BarTimeFrame.Hour));
+                var vtibars = await client.ListHistoricalBarsAsync(new HistoricalBarsRequest("VTI", from, into, BarTimeFrame.Hour));
+                var quote = await client.GetLatestQuoteAsync("AAPL");
+                var ask = (double)quote.AskPrice;
+                var bid = (double)quote.BidPrice;
+
+                var items = vtibars.Items;
+
+                Console.WriteLine("Test");
+
+                for (int i = 0; i < items.Count(); i++)
+                {
+                    Console.WriteLine("Bar Number : " + i);
+                    Console.WriteLine("High : " + (double)items[i].High);
+                    Console.WriteLine("Low : " + (double)items[i].Low);
+                    Console.WriteLine();
+                }
+                Console.Write("Quote AAPL : " + ask + ", " + bid);
+            }
+            catch(Exception e)
+            {
+                e.OutputError();
+            }
+
+            // Get our account information.
+
+            // Check if our account is restricted from trading.
+            /*
+            if (account.IsTradingBlocked)
+            {
+                Console.WriteLine("Account is currently restricted from trading.");
+            }
+            Console.WriteLine(account.BuyingPower + " is available as buying power.");
+            */
+            Console.Read();
+        }
     }
 }
