4832b218996b3beeb95b42be7a1479031674d4d4
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2021-09-22 07:39:43 -0700

    .

---
+++ CC_Library/Predictions/StonkPrediction/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_Library.Predictions.StonkPrediction
+{
+    /// <summary>
+    /// index of stonk in enum [enum size]
+    /// % change of stonk (price at close - price at open) / price at close
+    /// variability ratio (high - low) / price at close
+    /// secondary ratio (price at close - vwap / price at close
+    /// hours since change
+    /// Volume
+    /// </summary>
+    [Serializable]
+    class StonkValues
+    {
+        int[] indices;
+        double[] change;
+        double[] variability;
+        double[] secondary;
+        double[] hours;
+        double[] volume;
+    }
+}
