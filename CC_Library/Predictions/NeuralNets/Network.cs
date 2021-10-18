e61e14de4940557c47a19f3f337e29509e090014
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2021-10-18 07:56:02 -0700

    .

--- CC_Library/Predictions/NeuralNets/Network.cs
+++ CC_Library/Predictions/NeuralNets/Network.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using CC_Library.Datatypes;
 
 namespace CC_Library.Predictions
@@ -10,6 +11,24 @@ namespace CC_Library.Predictions
         public List<Layer> Layers { get; set; }
         public string Datatype { get; }
 
+        public double[] Predict(double[] results)
+        {
+            for(int i = 0; i < Layers.Count(); i++)
+            {
+                results = Layers[i].Output(results);
+            }
+            return results;
+        }
+        public List<double[]> Forward(double[] input)
+        {
+            List<double[]> Results = new List<double[]>();
+            Results.Add(input);
+            for (int k = 0; k < Layers.Count(); k++)
+            {
+                Results.Add(Layers[k].Output(Results.Last()));
+            }
+            return Results;
+        }
         public NeuralNetwork(Datatype datatype)
         {
             this.Layers = new List<Layer>();
