de8c7be953aefc55f8311e8651a14aa4facbd3fa
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-10-25 10:28:07 -0700

    Update AAPLNetwork.cs

--- CC_Library/Predictions/Prediction - Library/AAPLNetwork.cs
+++ CC_Library/Predictions/Prediction - Library/AAPLNetwork.cs
@@ -15,8 +15,7 @@ namespace CC_Library.Predictions
         private const double dropout = 0.1;
         public Datatype datatype { get { return Datatype.AAPL; } }
         public NeuralNetwork Network { get; }
-        //public NeuralNetwork MaxNetwork { get; }
-        //public NeuralNetwork MinNetwork { get; }
+        
         public AppleNetwork()
         {
             Network = datatype.LoadNetwork();
@@ -30,27 +29,6 @@ namespace CC_Library.Predictions
                 Network.Layers.Add(new Layer(Stonk.MktSize, Network.Layers.Last().Weights.GetLength(0), Activation.LRelu));
                 Network.Layers.Add(new Layer(3, Network.Layers.Last().Weights.GetLength(0), Activation.SoftMax));
             }
-            /*
-            MaxNetwork = Datatype.MaxValue.LoadNetwork(datatype);
-            if (MaxNetwork.Datatype == Datatype.None.ToString())
-            {
-                MaxNetwork = new NeuralNetwork(Datatype.MaxValue);
-                MaxNetwork.Layers.Add(new Layer(Stonk.MktSize, Stonk.MktSize, Activation.LRelu));
-                MaxNetwork.Layers.Add(new Layer(Stonk.MktSize, MaxNetwork.Layers.Last().Weights.GetLength(0), Activation.LRelu));
-                MaxNetwork.Layers.Add(new Layer(Stonk.MktSize, MaxNetwork.Layers.Last().Weights.GetLength(0), Activation.LRelu));
-                MaxNetwork.Layers.Add(new Layer(24, MaxNetwork.Layers.Last().Weights.GetLength(0), Activation.SoftMax));
-            }
-            
-            MinNetwork = Datatype.MinValue.LoadNetwork(datatype);
-            if (MinNetwork.Datatype == Datatype.None.ToString())
-            {
-                MinNetwork = new NeuralNetwork(Datatype.MinValue);
-                MinNetwork.Layers.Add(new Layer(Stonk.MktSize, Stonk.MktSize, Activation.LRelu));
-                MinNetwork.Layers.Add(new Layer(Stonk.MktSize, MinNetwork.Layers.Last().Weights.GetLength(0), Activation.LRelu));
-                MinNetwork.Layers.Add(new Layer(Stonk.MktSize, MinNetwork.Layers.Last().Weights.GetLength(0), Activation.LRelu));
-                MinNetwork.Layers.Add(new Layer(24, MinNetwork.Layers.Last().Weights.GetLength(0), Activation.SoftMax));
-            }
-            */
         }
         public int Predict(List<StonkValues> vals, WriteToCMDLine write)
         {
@@ -79,45 +57,28 @@ namespace CC_Library.Predictions
             StonkContext ctxt = new StonkContext(datatype);
             StonkMem sm = new StonkMem(comps.Count());
 
-            //NetworkMem MaxAAPLMem = new NetworkMem(MaxNetwork);
-            //NetworkMem MinAAPLMem = new NetworkMem(MinNetwork);
             NetworkMem AAPLMem = new NetworkMem(Network);
             NetworkMem StkMem = new NetworkMem(stk.Network);
             NetworkMem CtxtMem = new NetworkMem(ctxt.Network);
 
             var MktOutput = stk.Forward(comps, ctxt, sm);
             var F = Network.Forward(MktOutput, dropout, write);
-            //var MaxF = MaxNetwork.Forward(MktOutput, dropout, write);
-            //var MinF = MinNetwork.Forward(MktOutput, dropout, write);
 
             write("Predictions : " + F.Last().GetRank(0).GenText());
-            //write("Min Predictions : " + MinF.Last().GetRank(0).GenText());
 
             write("F Desired : " + max.GenText());
-            //write("Min Desired : " + min.GenText());
             
             var Error = CategoricalCrossEntropy.Forward(F.Last().GetRank(0), max);
             write("Max Error : " + Error.GenText());
-            /*
-            var MinError = CategoricalCrossEntropy.Forward(MinF.Last().GetRank(0), min);
-            write("Min Error : " + MinError.GenText());
-            */
             write("");
 
             var D = Network.Backward(F, max, AAPLMem, write);
-            //var MinD = MinNetwork.Backward(MinF, min, MinAAPLMem, write);
-
             stk.Backward(D, ctxt, sm, StkMem, CtxtMem);
-            //stk.Backward(MinD, ctxt, sm, StkMem, CtxtMem);
 
             AAPLMem.Update(1, 0.01, Network);
-            //MinAAPLMem.Update(1, 0.1, MinNetwork);
             StkMem.Update(1, 0.01, stk.Network);
             CtxtMem.Update(1, 0.01, ctxt.Network);
-            /*
-            MaxNetwork.Save();
-            MinNetwork.Save();
-            */
+            
             Network.Save();
             stk.Network.Save();
             ctxt.Save();
