ef7d80a48e261911756d349bb7a5f0e883fe9e9a
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-11-03 09:01:36 -0700

    Update Alpha.cs

--- CC_Library/Predictions/Prediction - Alpha/Alpha.cs
+++ CC_Library/Predictions/Prediction - Alpha/Alpha.cs
@@ -67,8 +67,8 @@ namespace CC_Library.Predictions
                 for (int i = Network.Layers.Count() - 1; i >= 0; i--)
                 {
                     ldv = mem.Layers[i].DActivation(ldv, am.LocationOutputs[j][i + 1]);
-                    mem.Layers[i].DBiases(ldv, Network.Layers[i]);
-                    mem.Layers[i].DWeights(ldv, am.LocationOutputs[j][i], Network.Layers[i]);
+                    mem.Layers[i].DBiases(ldv, Network.Layers[i], s.Length);
+                    mem.Layers[i].DWeights(ldv, am.LocationOutputs[j][i], Network.Layers[i], s.Length);
                     ldv = mem.Layers[i].DInputs(ldv, Network.Layers[i]);
                 }
             });
