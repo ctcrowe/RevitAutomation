7dac8ac49670e5a5380426f9b7247be9cd00ef61
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2021-11-08 07:56:29 -0800

    .

---
+++ Trader/ValueSet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CC_Library.Predictions;
+
+namespace Trader
+{
+    [Serializable]
+    public class ValueSet
+    {
+        public List<StonkValues> bars { get; set; }
+        public StonkValues TargetBar { get; set; }
+        public int Output { get; set; }
+
+        public ValueSet()
+        {
+            this.bars = new List<StonkValues>();
+            this.Output = 0;
+        }
+        public void SaveTxt()
+        {
+
+        }
+        public ValueSet LoadTxt(string fn)
+        {
+            return new ValueSet();
+        }
+        public void SaveBin()
+        {
+
+        }
+    }
+}
