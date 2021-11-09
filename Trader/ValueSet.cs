02964f7a28ede9fde486d2128b2ac9f80a46ac5d
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2021-11-09 09:10:03 -0800

    Update ValueSet.cs

--- Trader/ValueSet.cs
+++ Trader/ValueSet.cs
@@ -8,6 +8,7 @@ namespace Trader
     [Serializable]
     public class ValueSet
     {
+        public GUID guid { get; set; }
         public List<StonkValues> bars { get; set; }
         public StonkValues TargetBar { get; set; }
         public int Output { get; set; }
@@ -19,7 +20,8 @@ namespace Trader
         }
         public void SaveTxt()
         {
-
+            List<string> Lines = new List<string>();
+            Lines.Add("Input GUID " + guid.ToString());
         }
         public ValueSet LoadTxt(string fn)
         {
