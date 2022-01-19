c83cd3cf2fb998a024f920d410f2144efa0ffbe7
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2022-01-19 08:51:39 -0800

    Update Class1.cs

--- CC_Patterns/Class1.cs
+++ CC_Patterns/Class1.cs
@@ -19,12 +19,36 @@ namespace CC_Patterns
             RibbonPanel Panel = uiApp.CreateRibbonPanel(TabName, PanelName);
             TextBoxData tbd = new TextBoxData("Pattern Entry");
             ComboBoxData cbd = new ComboBoxData("Pattern Type");
-            
             var items = Panel.AddStackedItems(cbd, tbd);
-            items[1].AddItem(new ComboBoxMemberData("Brick Pattern", "Brick Pattern"));
-            items[1].AddItem(new ComboBoxMemberData("Herringbone Pattern", "Herringbone Pattern"));
+            var cbox = items[0] as ComboBox;
+            var tbox = items[1] as TextBox;
+            
+            cbox.AddItem(new ComboBoxMemberData("Brick Pattern", "Brick Pattern"));
+            cbox.AddItem(new ComboBoxMemberData("Herringbone Pattern", "Herringbone Pattern"));
             
             return Result.Succeeded;
+        }
+        private static void EnterPressed(object sender, TextBoxEnterPressedEventArgs args)
+        {
+            TextBox textBox = sender as TextBox;
+            string text = textBox.Value as string;
+            var combotype = args.Application.GetComboData();
+            combotype.CreatePattern(text);
+        }
+        private static string GetComboData(this UIApplication app)
+        {
+            string val = "";
+            try
+            {
+                var panels = app.GetRibbonPanels(TabName);
+                var panel = panels.Where(x => x.Name == PanelName).First();
+                var items = panel.GetItems();
+                var item = items.Where(x => x.ItemType == RibbonItemType.ComboBox).First();
+                var cb = item as ComboBox;
+                val = cb.Current.Name;
+            }
+            catch (Exception e) { e.OutputError(); }
+            return val;
         }
         public Result OnShutdown(UIControlledApplication uiApp)
         {
