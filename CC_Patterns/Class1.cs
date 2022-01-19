114334a638f24570ae63e1f985391bb5165a2e35
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2022-01-19 08:45:21 -0800

    Update Class1.cs

--- CC_Patterns/Class1.cs
+++ CC_Patterns/Class1.cs
@@ -12,12 +12,18 @@ namespace CC_Patterns
 {
     public class CC_PatternRibbon : IExternalApplication
     {
-        private const string tabName = "CCrowe";
+        private const string TabName = "CCrowe";
+        private const string PanelName = "Patterns";
         public Result OnStartup(UIControlledApplication uiApp)
         {
-            uiApp.CreateRibbonTab(tabName);
-            CCPaintPanel.PaintPanel(uiApp);
-            UpdateTab.CreatePanel(uiApp);
+            RibbonPanel Panel = uiApp.CreateRibbonPanel(TabName, PanelName);
+            TextBoxData tbd = new TextBoxData("Pattern Entry");
+            ComboBoxData cbd = new ComboBoxData("Pattern Type");
+            
+            var items = Panel.AddStackedItems(cbd, tbd);
+            items[1].AddItem(new ComboBoxMemberData("Brick Pattern", "Brick Pattern"));
+            items[1].AddItem(new ComboBoxMemberData("Herringbone Pattern", "Herringbone Pattern"));
+            
             return Result.Succeeded;
         }
         public Result OnShutdown(UIControlledApplication uiApp)
