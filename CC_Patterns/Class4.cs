4c322dbe04852b3f860ae02000e99f7c4ae3beb2
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2022-01-19 15:01:17 -0800

    Update Class4.cs

--- CC_Patterns/Class4.cs
+++ CC_Patterns/Class4.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// BoundsRatio = Height / Width
+// Slope Tolerance = 25? Degrees?
+// RiseSeq = a list of numbers where the step size is rise / run and the list contains 25 numbers (tolerance)
+// RunSeq = a list of numbers where the step size is 1 and the list contains 25 numbers (tolerance)
+// Rise = Rise Seq repeated tolerance times, 
+
+
 namespace CC_Patterns
 {
     class Class4
