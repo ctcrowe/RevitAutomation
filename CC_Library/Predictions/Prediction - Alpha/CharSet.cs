6c30b9c57c8dee08077585657e2cacc4118a46db
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2022-01-28 10:45:57 -0800

    Update CharSet.cs

--- CC_Library/Predictions/Prediction - Alpha/CharSet.cs
+++ CC_Library/Predictions/Prediction - Alpha/CharSet.cs
@@ -9,13 +9,13 @@ namespace CC_Library.Predictions
     internal static class CharSet
     {
         public const int CharCount = 38;
-        private static char[] Chars = new char[38] {
-            'A', 'B', 'C', 'D', 'E', 'F', 'G',
-            'H', 'I', 'J', 'K', 'L', 'M', 'N',
-            'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-            'V', 'W', 'X', 'Y', 'Z', '0', '1',
-            '2', '3', '4', '5', '6', '7', '8',
-            '9', ' ', '_'};
+        private static Dictionary<char, int> Chars = new Dictionary<char, int> {
+            ('A', 2), ('B', 2), ('C', 2), ('D', 2), ('E', 2), ('F', 2), ('G', 2),
+            ('H', 5), ('I', 1), ('J', 6), ('K', 4), ('L', 4), ('M', M), ('N', 3),
+            ('O', 4), ('P', 5), ('Q', 10), ('R', 1), ('S', 1), ('T', 1), ('U', 2),
+            ('V', 8), ('W', 2), ('X', 8), ('Y', 3), ('Z', 10), ('0', 0), ('1', 0),
+            ('2', 0), ('3', 0), ('4', 0), ('5', 0), ('6', 0), ('7', 0), ('8', 0),
+            ('9', 0), (' ', 0), ('_', 0)};
 
         public static double[] Locate(this string s, int numb, int range)
         {
@@ -31,7 +31,11 @@ namespace CC_Library.Predictions
 
             return result;
         }
-        private static int LocationOf(char c) { return Chars.Contains(c) ? Chars.ToList().IndexOf(c) : Chars.Count() - 1; }
+        private static int LocationOf(char c) { return Chars.Keys.Contains(c) ? Chars.Keys.ToList().IndexOf(c) : Chars.Keys.Count() - 1; }
+        private static int LocationOfS(char c) { return Chars.Keys.Contains(c) ? Chars[c] : 0; }
+        private static int LocationOfC(char c) { return !Chars.Keys.Contains(c) ? Chars.Count() - 1 :
+                                                Chars[c] > 0? Chars.Keys.ToList().IndexOf(c) :
+                                                Chars.Keys.Count() - 1; }
     }
     public static class TestCharSet
     {
