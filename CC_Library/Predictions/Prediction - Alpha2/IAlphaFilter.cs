5541f964288164476f55804e29698e44e7c31c75
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2022-02-07 09:07:38 -0800

    Update IAlphaFilter.cs

--- CC_Library/Predictions/Prediction - Alpha2/IAlphaFilter.cs
+++ CC_Library/Predictions/Prediction - Alpha2/IAlphaFilter.cs
@@ -5,6 +5,7 @@
         NeuralNetwork AttentionNetwork { get; }
         NeuralNetwork ValueNetwork { get; }
         int GetSize();
+        double GetChangeSize();
         double[] Forward(string s, AlphaMem am);
         void Backward(string s, double[] DValues,
             AlphaMem am, NetworkMem mem, NetworkMem CtxtMem);
