fac0c50c0ee993c47869dda499d232e2b2631c46
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2022-03-07 14:54:30 -0800

    Update IAlphaFilter.cs

--- CC_Library/Predictions/Prediction - Alpha2/IAlphaFilter.cs
+++ CC_Library/Predictions/Prediction - Alpha2/IAlphaFilter.cs
@@ -8,6 +8,6 @@
         int GetLength(string s, NeuralNetwork net);
         double GetChangeSize();
         double[][][][][] Forward(string s, NeuralNetwork net);
-        void Backward(string s, double[] DValues, double[][][][][] outputs, NetworkMem ValMem, NetworkMem FocMem);
+        void Backward(double[] DValues, double[][][][][] outputs, NetworkMem ValMem, NetworkMem FocMem);
     }
 }
