8cd2ccbc7ab9a5da3caf58e989c630b2af62038b
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2022-03-17 14:42:13 -0700

    Update OutputPredictions.cs

--- CC_Library/Predictions/OutputPredictions.cs
+++ CC_Library/Predictions/OutputPredictions.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Windows.Forms;
 using CC_Library.Datatypes;
 using System.IO;
-/*
 namespace CC_Library.Predictions
 {
     public static class PredOutput
@@ -12,42 +11,29 @@ namespace CC_Library.Predictions
         {
             OpenFileDialog ofd = new OpenFileDialog()
             {
-                FileName = "Select a csv file",
-                Filter = "CSV files (*.csv)|*.csv",
-                Title = "Open csv file"
+                FileName = "Select a bin file",
+                Filter = "CSV files (*.bin)|*.bin",
+                Title = "Open bin file"
             };
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 var filepath = ofd.FileName;
-                Random random = new Random();
-
-                if (Enum.GetNames(typeof(Datatype)).Any(x => filepath.Contains(x)))
+                write("Network Type : " + datatype.ToString());
+                var Lines = File.ReadAllLines(filepath);
+                if (sfd.ShowDialog() == DialogResult.OK)
                 {
-                    Datatype datatype = (Datatype)Enum.Parse(typeof(Datatype), Enum.GetNames(typeof(Datatype)).Where(x => filepath.Contains(x)).First());
-                    write("Network Type : " + datatype.ToString());
-                    var Lines = File.ReadAllLines(filepath);
+                    var fp = sfd.FileName;
 
-                    SaveFileDialog sfd = new SaveFileDialog()
+                    switch (datatype)
                     {
-                        FileName = "Create a txt file",
-                        Filter = "TXT files (*.txt)|*.txt",
-                        Title = "Create a txt file"
-                    };
-                    if (sfd.ShowDialog() == DialogResult.OK)
-                    {
-                        var fp = sfd.FileName;
-
-                        switch (datatype)
-                        {
-                            default:
-                            case Datatype.Masterformat:
-                                string[] r = MasterformatNetwork.PredictAll(Lines, write);
-                                File.WriteAllLines(fp, r);
-                                break;
-                        }
+                        default:
+                        case Datatype.Masterformat:
+                            string[] r = MasterformatNetwork.PredictAll(Lines, write);
+                            File.WriteAllLines(fp, r);
+                        break;
                     }
                 }
             }
         }
     }
-}*/
+}
