a853a23b3731b2c52811fb7b0442b756b2127a55
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2022-03-22 22:12:12 -0700

    .

--- CC_Library/Predictions/Prediction - Alpha2/WordFilter.cs
+++ CC_Library/Predictions/Prediction - Alpha2/WordFilter.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CC_Library;
 using CC_Library.Datatypes;
-
+//revise to find length from larger search radius (running once) and then add entire range to final definition of word.
 
 namespace CC_Library.Predictions
 {
