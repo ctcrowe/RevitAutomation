a1694fce1226735103d0d462e7756979c3638bf9
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2022-04-22 07:52:03 -0700

    .

--- CC_Library/Predictions/Prediction - Alpha/CharSet.cs
+++ CC_Library/Predictions/Prediction - Alpha/CharSet.cs
@@ -7,6 +7,10 @@ namespace CC_Library.Predictions
 {
     internal class AlphaHash
     {
+        public static Dictionary<string, int> Hash = new Dictionary<string, int>()
+        {
+            {"AAA", 1}, {"AAB", 2}, {"AAC", 3}, {"AAD", 4}, {"AAE", 5}
+        };
         /*
         string s = "";
         int n = 0;
@@ -24,6 +28,15 @@ namespace CC_Library.Predictions
             }
         }
         */
+    }
+    public enum Chars
+    {
+        A = 1, B = 2, C = 3, D = 4, E = 5,
+        F = 6, G = 7, H = 8, I = 9, J = 10,
+        K = 11, L = 12, M = 13, N = 14, O = 15,
+        P = 16, Q = 17, R = 18, S = 19, T = 20,
+        U = 21, V = 22, W = 23, X = 24, Y = 25,
+        Z = 26, _ = 27
     }
     internal static class CharSet
     {
