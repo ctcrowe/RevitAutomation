7213e783a8cdd3202cffbd8d1c1eddc54ee46ec9
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2022-05-17 13:47:27 -0700

    Update WordFilter - Copy.cs

--- CC_Library/Predictions/Prediction - Alpha2/WordFilter - Copy.cs
+++ CC_Library/Predictions/Prediction - Alpha2/WordFilter - Copy.cs
@@ -5,15 +5,6 @@ using System.Threading.Tasks;
 using CC_Library;
 using CC_Library.Datatypes;
 
-        //[][][][][x] => value set
-        //[][][][x] => dropout / no. 2 wide always
-        //[][][x] => layer number - based on Filter Size (# of layers)
-        //[][x] => location location location - this version will make a list and then convert it to an array.
-        //[x] => layer group - 3 wide always - 0 = locations, 1 = context, 2 = combined output
-
-        //[2][1][1][3][x] =>
-        //  x = [0] = locations, ,[1] = locations, [2] = const int Size above
-
 namespace CC_Library.Predictions
 {
 }
