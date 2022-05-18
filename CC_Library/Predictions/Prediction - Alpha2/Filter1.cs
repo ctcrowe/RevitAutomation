a9d7e24a79df217488d8efcd539914d395a03f93
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2022-05-18 09:43:13 -0700

    Update Filter1.cs

--- CC_Library/Predictions/Prediction - Alpha2/Filter1.cs
+++ CC_Library/Predictions/Prediction - Alpha2/Filter1.cs
@@ -1,12 +1,1 @@
-using System.Collections.Generic;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-using CC_Library;
-using CC_Library.Datatypes;
-using CC_Library.Predictions;
 
-
-namespace CC_Library.Predictions
-{
-}
