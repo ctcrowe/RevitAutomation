1a1954de01ffd9bd0e7aa11678bdcc97a8c22924
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2022-05-23 20:54:20 -0700

    .

--- CC_Library/Predictions/Prediction - Library/MasterformatNetwork.cs
+++ CC_Library/Predictions/Prediction - Library/MasterformatNetwork.cs
@@ -1,1 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using CC_Library.Datatypes;
 
+namespace CC_Library.Predictions
+{
+    public static class MasterformatNetwork
+    {
+        private const double dropout = 0.1;
+        private const double rate = 0.1;
+        public static double[] Predict(string s, WriteToCMDLine write)
+        {
+            var Alpha = new Alpha(write);
+            var MF = "Masterformat9".LoadXfmr(Alpha._Outputs, 40, 80, write);
+            var AlphaMem = Alpha.GetMem();
+            var MFMem = new AttentionMem();
+
+            var AOut = Alpha.Forward(s, AlphaMem, write);
+            var MFOut = MF.Forward(AOut);
+            var output = MFOut.SumRange();
+            output = Activations.SoftMax(output);
+            return output;
+        }
+        public static double[] Propogate
+            (string[] Samples, WriteToCMDLine write, bool tf = false)
+        {
+            var results = new double[2];
+            var Alpha = new Alpha(write);
+            var Rates = Alpha.GetChange();
+
+            var MF = "Masterformat9".LoadXfmr(Alpha._Outputs, 40, 80, write);
+            var MFRate = new AttentionChange(MF);
+
+            try
+            {
+                double[] max = new double[Samples.Count()];
+                double[] final = new double[Samples.Count()];
+                double[] outputs = new double[Samples.Count()];
+                double[] desouts = new double[Samples.Count()];
+
+                Parallel.For(0, Samples.Count(), j =>
+                {
+                    var AlphaMem = Alpha.GetMem();
+                    var MFMem = new AttentionMem();
+
+                    var AlphaOut = Alpha.Forward(Samples[j].Split(',').First(), AlphaMem, write);
+                    MF.Forward(AlphaOut, MFMem);
+
+                    var F = Activations.SoftMax(MFMem.attention);
+
+                    max[j] = F[F.ToList().IndexOf(F.Max())];
+                    outputs[j] = F.ToList().IndexOf(F.Max());
+                    final[j] = F[int.Parse(Samples[j].Split(',').Last())];
+                    desouts[j] = int.Parse(Samples[j].Split(',').Last());
+
+                    var DesiredOutput = new double[40];
+                    DesiredOutput[int.Parse(Samples[j].Split(',').Last())] = 1;
+                    results[0] += CategoricalCrossEntropy.Forward(F, DesiredOutput).Max();
+                    results[1] += F.ToList().IndexOf(F.Max()) == int.Parse(Samples[j].Split(',').Last()) ? 1 : 0;
+
+                    var DValues = Activations.InverseCombinedCrossEntropySoftmax(DesiredOutput, F);
+                    var dvals = DValues.Dot(MFMem.attn.Ones()); //returns a vector [s.Length, size]
+                    dvals = MF.Backward(MFMem, MFRate, dvals);
+                    Alpha.Backward(dvals, AlphaMem, Rates, write);
+                });
+                final.WriteArray("Desired Output", write);
+                max.WriteArray("Max Output", write);
+                outputs.WriteArray("Outputs", write);
+                desouts.WriteArray("Desired", write);
+            }
+            catch (Exception e) { e.OutputError(); }
+            //MFMem.Update(Samples.Count(), rate, net);
+            Alpha.Update(Rates, write);
+            MF.Update(MFRate, write);
+
+            results[0] /= Samples.Count();
+            results[1] /= Samples.Count();
+
+            write("Run Error : " + results[0]);
+            write("Run Accuracy : " + results[1]);
+            //net.Save();
+            string Folder = "NeuralNets".GetMyDocs();
+            MF.Save(Folder);
+            return results;
+        }
+    }
+}
