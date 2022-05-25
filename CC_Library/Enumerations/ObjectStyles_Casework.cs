c6c2df5d6053d87c2b1f28cadbf3b335afbc4269
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2022-05-24 19:04:09 -0700

    .

---
+++ CC_Library/Enumerations/ObjectStyles_Casework.cs
@@ -0,0 +1,15 @@
+﻿namespace CC_Library
+{
+    public enum ObjectStyles_Casework
+    {
+        _Zero_,
+        Base,
+        Countertop,
+        Lower_Cabinet,
+        Upper_Cabinet,
+        Glass,
+        Hardware,
+        Hidden_Lines,
+        ADA
+    }
+}
