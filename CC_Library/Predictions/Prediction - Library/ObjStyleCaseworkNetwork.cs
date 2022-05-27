718d74f54c6de162f0ba6a255f2d142091a3c670
ctcrowe <56172740+ctcrowe@users.noreply.github.com>
2022-05-27 13:30:53 -0700

    Update ObjStyleCaseworkNetwork.cs

--- CC_Library/Predictions/Prediction - Library/ObjStyleCaseworkNetwork.cs
+++ CC_Library/Predictions/Prediction - Library/ObjStyleCaseworkNetwork.cs
@@ -7,6 +7,90 @@ namespace CC_Library.Predictions
 {
     public static class ObjStyleCaseworkNetwork
     {
+/*
+//  New concept : load Xfmr based on Type t, rather than just outright loading it. This will add a little  complexity here, but simplify elsewhere.
+//  Enum.GetNames(typeof(ObjectCategory)).ToList()
+    public static class ObjStyleNetworks
+    {
+        public static double[] Predict(string s, Type t, WriteToCMDLine write)
+        {
+            var Alpha = new Alpha(write);
+            string Name = t.Name;
+            var count = Enum.GetNames(t).ToList().Count();
+            var Obj = Name.LoadXfmr(Alpha._Outputs, count, 80, write);
+
+            var AOut = Alpha.Forward(s, write);
+            var ObjOut = Obj.Forward(AOut);
+            var output = ObjOut.SumRange();
+            output = Activations.SoftMax(output);
+            return output;
+        }
+        public static double[] Propogate
+            (string[] Samples, Type t, WriteToCMDLine write, bool tf = false)
+        {
+            var results = new double[2];
+            var Alpha = new Alpha(write);
+            var Rates = Alpha.GetChange();
+
+            string Name = t.Name;
+            var count = Enum.GetNames(t).ToList().Count();
+            var Obj = Name.LoadXfmr(Alpha._Outputs, count, 80, write);
+            var ObjRate = new AttentionChange(Obj);
+
+            try
+            {
+                double[] max = new double[Samples.Count()];
+                double[] final = new double[Samples.Count()];
+                double[] outputs = new double[Samples.Count()];
+                double[] desouts = new double[Samples.Count()];
+
+                Parallel.For(0, Samples.Count(), j =>
+                {
+                    var AlphaMem = Alpha.GetMem();
+                    var ObjMem = new AttentionMem();
+
+                    var AlphaOut = Alpha.Forward(Samples[j].Split(',').First(), AlphaMem, write);
+                    Obj.Forward(AlphaOut, ObjMem);
+
+                    var F = Activations.SoftMax(ObjMem.attention);
+
+                    max[j] = F[F.ToList().IndexOf(F.Max())];
+                    outputs[j] = F.ToList().IndexOf(F.Max());
+                    final[j] = F[int.Parse(Samples[j].Split(',').Last())];
+                    desouts[j] = int.Parse(Samples[j].Split(',').Last());
+
+                    var DesiredOutput = new double[9];
+                    DesiredOutput[int.Parse(Samples[j].Split(',').Last())] = 1;
+                    results[0] += CategoricalCrossEntropy.Forward(F, DesiredOutput).Max();
+                    results[1] += F.ToList().IndexOf(F.Max()) == int.Parse(Samples[j].Split(',').Last()) ? 1 : 0;
+
+                    var DValues = Activations.InverseCombinedCrossEntropySoftmax(DesiredOutput, F);
+                    var dvals = DValues.Dot(ObjMem.attn.Ones()); //returns a vector [s.Length, size]
+                    dvals = Obj.Backward(ObjMem, ObjRate, dvals);
+                    Alpha.Backward(dvals, AlphaMem, Rates, write);
+                });
+                final.WriteArray("Desired Output", write);
+                max.WriteArray("Max Output", write);
+                outputs.WriteArray("Outputs", write);
+                desouts.WriteArray("Desired", write);
+            }
+            catch (Exception e) { e.OutputError(); }
+
+            //Alpha.Update(Rates, write);
+            Obj.Update(ObjRate, write);
+
+            results[0] /= Samples.Count();
+            results[1] /= Samples.Count();
+
+            write("Run Error : " + results[0]);
+            write("Run Accuracy : " + results[1]);
+
+            string Folder = "NeuralNets".GetMyDocs();
+            Obj.Save(Folder);
+            return results;
+        }
+    }
+*/
         public static double[] Predict(string s, WriteToCMDLine write)
         {
             var Alpha = new Alpha(write);
