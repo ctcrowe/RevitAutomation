444327a75490e0c9a14b9d914883f04bf462b1e6
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2022-06-05 08:05:09 -0700

    .

--- CC_Events/CC_RevitRibon.cs
+++ CC_Events/CC_RevitRibon.cs
@@ -18,34 +18,20 @@ namespace CC_Plugin
             CCPaintPanel.PaintPanel(uiApp);
             UpdateTab.CreatePanel(uiApp);
             ObjStylesTab.ObjTab(uiApp, tabName);
-            //FamilyReorganize.Run();
 
-            //DetailPanel.DetailTab(uiApp, tabName);
-            //QCTab.QCPanel(uiApp, tabName);
-            // MFPanel.MFTab(uiApp, tabName);
-            //AnalysisTab.AnalysisPanel(uiApp, tabName);
-            //SchduleTab.SchedulePanel(uiApp, tabName);
-            //uiApp.LoadFamPanel(tabName);
-
-            //try { LineStyleUpdater.OnStartup(uiApp); } catch (Exception e) { e.OutputError(); }
             try { LineStyleNetworkUpdater.OnStartup(uiApp); } catch (Exception e) { e.OutputError(); }
-            try { MFTypeNameChange.OnStartup(uiApp); } catch (Exception e) { e.OutputError(); }
             try { TypeNamingUpdater.OnStartup(uiApp); } catch (Exception e) { e.OutputError(); }
             try { DocumentSaved.OnStartup(uiApp); } catch (Exception e) { e.OutputError(); }
             try { PlaneTypeUpdater.OnStartup(uiApp); } catch (Exception e) { e.OutputError(); }
-            //try { ObjStyleUpdater.OnStartup(uiApp); } catch (Exception e) { e.OutputError(); }
             try { ObjStyleNetworkUpdater.OnStartup(uiApp); } catch (Exception e) { e.OutputError(); }
             return Result.Succeeded;
         }
         public Result OnShutdown(UIControlledApplication uiApp)
         {
-            //LineStyleUpdater.OnShutdown(uiApp);
             LineStyleNetworkUpdater.OnShutdown(uiApp);
-            MFTypeNameChange.OnShutdown(uiApp);
             TypeNamingUpdater.OnShutdown(uiApp);
             DocumentSaved.OnShutdown(uiApp);
             PlaneTypeUpdater.OnShutdown(uiApp);
-            //ObjStyleUpdater.OnShutdown(uiApp);
             ObjStyleNetworkUpdater.OnShutdown(uiApp);
             return Result.Succeeded;
         }
