6fa9c7dc1f255870f05a7323d86e079dfb207eee
BIM-Nest <56172740+BIM-Nest@users.noreply.github.com>
2022-06-06 16:10:53 -0700

    .

---
+++ CC_Library/Enumerations/ObjectStyles/ObjectStyles_PlumbingFixtures.cs
@@ -0,0 +1,15 @@
+﻿namespace CC_Library
+{
+    public enum ObjectStyles_PlumbingFixtures
+    {
+        _Zero_,
+        Bathtub,
+        Sink,
+        Lavatory,
+        Faucet,
+        Water_Closet,
+        Shower,
+        Drain,
+        Rain_Leader
+    }
+}
