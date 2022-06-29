99761e8419e70871df8bfd2722311c35f5d6f78d
Charlie Crowe <charlietcrowe@gmail.com>
2022-06-29 13:12:58 -0700

    Update DotProduct.js

--- PredictiveLearning/DotProduct.js
+++ PredictiveLearning/DotProduct.js
@@ -1,7 +1,30 @@
+let a = [[1,2],[3,4]];
+let b = [[5,6],[7,8]];
+try {
+  alert(Dot(a, b));
+}
+catch(err) { alert(err.name); }
+
+
 function Dot(x, y) {
-    if (Array.IsArray(x) && Array.IsArray(y)) {
-        
-        Promise.all()
+  if(!Array.isArray(x)) { return "X is not an Array"; }
+  if(!Array.isArray(y)) { return "Y is not an Array"; }
+
+  const result = new Array(x.length);
+  for(let ylen = 0; ylen < result.length; ylen++) {
+    result[ylen] = new Array(y[0].length);
+    for(let ytot = 0; ytot < result[ylen].length; ytot++) {
+      result[ylen][ytot] = 0;
     }
-    return 0;
+  }
+
+  for(let i = 0; i < x.length; i++) {
+    for(let j = 0; j < y.length; j++) {
+      for(let k = 0; k < y[0].length; k++) {
+        result[i][k] += x[i][j] * y[j][k];
+      }
+    }
+  }
+
+  return result;
 }
