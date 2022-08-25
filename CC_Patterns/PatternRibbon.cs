9701cf454405301ae4f5e2cfecd492024ac35e41
Charlie Crowe <charlietcrowe@gmail.com>
2022-08-25 10:18:57 -0700

    Update PatternRibbon.cs

--- CC_Patterns/PatternRibbon.cs
+++ CC_Patterns/PatternRibbon.cs
@@ -16,11 +16,20 @@ namespace CC_Patterns
         {
             try { uiApp.CreateRibbonTab(TabName); } catch {};
             RibbonPanel Panel = uiApp.CreateRibbonPanel(TabName, PanelName);
-            TextBoxData tbd = new TextBoxData("Pattern Entry");
+            TextBoxData tbd1 = new TextBoxData("Width");
+            TextBoxData tbd2 = new TextBoxData("Height");
+            TextBoxData tbd3 = new TextBoxData("Grout Width");
+            TextBoxData tbd4 = new TextBoxData("Grout Height");
+            TextBoxData tbd5 = new TextBoxData("Spacing");
             ComboBoxData cbd = new ComboBoxData("Pattern Type");
-            var items = Panel.AddStackedItems(cbd, tbd);
-            var cbox = items[0] as ComboBox;
-            var tbox = items[1] as TextBox;
+            var items1 = Panel.AddStackedItems(cbd, tbd1, tbd2);
+            var items2 = Panel.AddStackedItems(tbd3, tbd4, tbd5);
+            var cbox = items1[0] as ComboBox;
+            var WidthTB = items1[1] as TextBox;
+            var HeightTB = items1[2] as TextBox;
+            var GroutWidthTB = items2[0] as TextBox;
+            var GroutHeightTB = items2[1] as TextBox;
+            var SpacingTB = items2[2] as TextBox;
             
             var Brick = new ComboBoxMemberData("Brick Pattern", "Brick Pattern");
             Brick.ToolTip = "Used for creating brick and tiling patterns of all types including a direct stack.";
@@ -40,9 +49,20 @@ namespace CC_Patterns
             
             cbox.AddItem(new ComboBoxMemberData("Custom Pattern", "Custom Pattern"));
 
-            tbox.Width = 250;
-            tbox.PromptText = "Width, Height, Grout, Steps";
-            tbox.EnterPressed += EnterPressed;
+            WidthTB.PromptText = "Width";
+            HeightTB.PromptText = "Height";
+            GroutWidthTB.PromptText = "Grout Width";
+            GroutHeightTB.PromptText = "Grout Height";
+            SpacingTB.PromptText = "Spacing";
+            
+            //tbox.Width = 250;
+            
+            WidthTB.EnterPressed += EnterPressed;
+            HeightTB.EnterPressed += EnterPressed;
+            GroutWidthTB.EnterPressed += EnterPressed;
+            GroutHeightTB.EnterPressed += EnterPressed;
+            SpacingTB.EnterPressed += EnterPressed;
+            
             cbox.CurrentChanged += CurrentChanged;
             return Result.Succeeded;
         }
@@ -54,28 +74,54 @@ namespace CC_Patterns
             {
                 default:
                 case "Brick Pattern":
-                    SetTB(args.Application, "Width, Height, Grout Width, Grout Height, Steps");
+                    SetTB(args.Application, "Width", "Height", "Grout Width", "Grout Height", "Steps");
                     break;
                 case "Herringbone Pattern":
-                    SetTB(args.Application, "Width, Height");
+                    SetTB(args.Application, "Width", "Height", "null", "null", "null");
                     break;
                 case "Basket Pattern":
-                    SetTB(args.Application, "Width, Height");
+                    SetTB(args.Application, "Width", "Height", "null", "null", "null");
                     break;
                 case "Plank Pattern":
-                    SetTB(args.Application, "Width, Height, Grout");
+                    SetTB(args.Application, "Width", "Height", "Grout", "null", "null");
                     break;
                 case "Drawn Pattern":
-                    SetTB(args.Application, "Draw Detail Lines");
+                    SetTB(args.Application, "null", "null", "null", "null", "null");
                     break;
             }
         }
         private static void EnterPressed(object sender, TextBoxEnterPressedEventArgs args)
         {
             TextBox textBox = sender as TextBox;
-            string text = textBox.Value as string;
+            string text = CreateTextData(args.Application);
             var combotype = GetComboData(args.Application);
             CreatePattern(args.Application.ActiveUIDocument.Document, combotype, text);
+        }
+        private static string CreateTextData(UIApplication app)
+        {
+            string val = "";
+            try
+            {
+                var panels = app.GetRibbonPanels(TabName);
+                var panel = panels.Where(x => x.Name == PanelName).First();
+                var items = panel.GetItems();
+                
+                var WidthBox = items.Where(x => x.ItemType == RibbonItemType.TextBox).Where(y => y.Name = "Width").First() as TextBox;
+                var HeightBox = items.Where(x => x.ItemType == RibbonItemType.TextBox).Where(y => y.Name = "Height").First() as TextBox;
+                var GWidthBox = items.Where(x => x.ItemType == RibbonItemType.TextBox).Where(y => y.Name = "Grout Width").First() as TextBox;
+                var GHeightBox = items.Where(x => x.ItemType == RibbonItemType.TextBox).Where(y => y.Name = "Grout Height").First() as TextBox;
+                var SpacingBox = items.Where(x => x.ItemType == RibbonItemType.TextBox).Where(y => y.Name = "Spacing").First() as TextBox;
+                
+                string w = WidthBox.Value as string;
+                string h = HeightBox.Value as string;
+                string gw = GWidthBox.Value as string;
+                string gh = GHeightBox.Value as string;
+                string s = SpacingBox.Value as string;
+                
+                val = w + "," + h + "," + gw + "," + gh + "," + s;
+            }
+            catch (Exception e) { }
+            return val;
         }
         private static string GetComboData(UIApplication app)
         {
@@ -92,19 +138,33 @@ namespace CC_Patterns
             catch (Exception e) { }
             return val;
         }
-        private static void SetTB(UIApplication app, string val)
+        private static void SetTB(UIApplication app, string v1, string v2. string v3, string v4, string v5)
         {
             try
             {
                 var panels = app.GetRibbonPanels(TabName);
                 var panel = panels.Where(x => x.Name == PanelName).First();
                 var items = panel.GetItems();
-                var item = items.Where(x => x.ItemType == RibbonItemType.TextBox).First();
-                var tb = item as TextBox;
-                tb.PromptText = val;
-                tb.Value = string.Empty;
+                
+                var WidthBox = items.Where(x => x.ItemType == RibbonItemType.TextBox).Where(y => y.Name = "Width").First() as TextBox;
+                var HeightBox = items.Where(x => x.ItemType == RibbonItemType.TextBox).Where(y => y.Name = "Height").First() as TextBox;
+                var GWidthBox = items.Where(x => x.ItemType == RibbonItemType.TextBox).Where(y => y.Name = "Grout Width").First() as TextBox;
+                var GHeightBox = items.Where(x => x.ItemType == RibbonItemType.TextBox).Where(y => y.Name = "Grout Height").First() as TextBox;
+                var SpacingBox = items.Where(x => x.ItemType == RibbonItemType.TextBox).Where(y => y.Name = "Spacing").First() as TextBox;
+                
+                
+                WidthBox.PromptText = v1;
+                WidthBox.Value = string.Empty;
+                HeightBox.PromptText = v2;
+                HeightBox.Value = string.Empty;
+                GWidthBox.PromptText = v3;
+                GWidthBox.Value = string.Empty;
+                GHeightBox.PromptText = v4;
+                GHeightBox.Value = string.Empty;
+                SpacingBox.PromptText = v5;
+                SpacingBox.Value = string.Empty;
             }
-            catch {}
+            catch (Exception e) { }
         }
         public static void CreatePattern(Document doc, string combotype, string text)
         {
