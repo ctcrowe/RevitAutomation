b3b0ab81652cac18848376f355b7b5a547452e2e
Charlie Crowe <charlietcrowe@gmail.com>
2022-08-30 17:07:53 -0700

    Update Params.cs

--- CC_Library/Parameters/Params.cs
+++ CC_Library/Parameters/Params.cs
@@ -2,6 +2,37 @@
 
 namespace CC_Library.Parameters
 {
+    public static class MCA_Params
+    {
+        public static readonly Param Masterformat = new Param
+            ("MCA_MF Division",
+            new Guid("aedbf7d2d73c44048cf7ed26f3231d25"),
+            Subcategory.Generic,
+            ParamType.Int,
+            false,
+            true);
+        public static readonly Param Occupancy = new Param
+            ("MCA_Occupancy Group",
+             new Guid("d9e989bf54144291ac599107f0bff748"),
+             Subcategory.Rooms,
+             ParamType.Text,
+             true,
+             true);
+        public static readonly Param OccupantLoadFactor = new Param
+            ("MCA_Occupant Load Factor",
+             new Guid("cd4aa818198e418dbef1ffae4492fb64"),
+             Subcategory.Rooms,
+             Paramtype.Area,
+             true,
+             true);
+        public static readonly Param MasterHeight = new Param
+            ("MCA_Height",
+             new Guid("2f97b5172f0b42888be2d20d2c817d26"),
+             Subcategory.Generic,
+             Paramtype.Length,
+             true,
+             true);
+    }
     public static class Params
     {
         public static readonly Param Masterformat = new Param
